3d483e9bbc875ea94ed1dfcc384f6f07d8e85364
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-09 22:45:11 +0100

    Query tokenizer done, a bunch of tests to fix left.

--- lib/card.rb
+++ lib/card.rb
@@ -12,6 +12,43 @@ class Card
     ["types", "subtypes", "supertypes"].map{|t| @data[t] || []}.flatten.map(&:downcase)
   end
 
+  def rarity
+    @data["rarity"].downcase
+  end
+
+  def legality(format)
+    leg = @data["legalities"].find{|leg| leg["format"].downcase == format}
+    if leg
+      leg["legality"].downcase
+    else
+      nil
+    end
+  end
+
+  def artist
+    @data["artist"].downcase
+  end
+
+  def cmc
+    @data["cmc"] || 0
+  end
+
+  def text
+    @data["text"] || ""
+  end
+
+  def flavor
+    @data["flavor"] || ""
+  end
+
+  def power
+    @data["power"] ?  @data["power"].to_i : nil
+  end
+
+  def toughness
+    @data["toughness"] ?  @data["toughness"].to_i : nil
+  end
+
   def method_missing(m)
     if @data.has_key?(m.to_s)
       @data[m.to_s]
