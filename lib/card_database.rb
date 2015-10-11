0983ae1c32a67046d7ce5584e121d696e9e7b8e4
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-11 03:34:02 +0100

    Color identity of multipart cards fixed

--- lib/card_database.rb
+++ lib/card_database.rb
@@ -33,6 +33,9 @@ class CardDatabase
   def initialize(path)
     @path = Pathname(path)
     @data = JSON.parse(@path.open.read)
+    @cards = {}
+    @ci = {}
+    parse_data!
   end
 
   def search(query_string)
@@ -47,11 +50,25 @@ class CardDatabase
   end
 
   def each_card
+    @cards.each do |name, printings|
+      printings.each do |printing|
+        yield printing
+      end
+    end
+  end
+
+  def cards
+    enum_for(:each_card).to_a
+  end
+
+  private
+
+  def parse_data!
     @data.each do |set_code, set|
       set_code = set["magicCardsInfoCode"] || set["code"]
       block = MagicBlocks.find{|c,n,*xx| xx.include?(set_code)} || []
       set["cards"].each do |card_data|
-        yield Card.new({
+        card = Card.new({
           "set_code" => set_code,
           "set_name" => set["name"],
           "block_code" => block[0],
@@ -59,11 +76,15 @@ class CardDatabase
           "border" => set["border"],
           "releaseDate" => set["releaseDate"],
         }.merge(card_data))
+        @cards[card.name] ||= []
+        @cards[card.name] << card
+        @ci[card.name] ||= card.partial_color_identity
       end
     end
-  end
-
-  def cards
-    enum_for(:each_card).to_a
+    each_card do |card|
+      if card.has_multiple_parts?
+        card.color_identity = card.names.map{|n| @ci[n]}.inject(&:|)
+      end
+    end
   end
 end
