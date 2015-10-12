3a7e7eae67b962b961cc6b98bc27ec2633503cf4
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-12 12:52:27 +0100

    Some performance tweaks

--- lib/card_set.rb
+++ lib/card_set.rb
@@ -1,15 +1,12 @@
 class CardSet
-  attr_reader :data
+  attr_reader :set_name, :set_code, :block_name, :block_code, :border, :release_date
   def initialize(data)
-    @data = data
-  end
-
-  def method_missing(m)
-    if @data.has_key?(m.to_s)
-      @data[m.to_s]
-    else
-      super
-    end
+    @set_name     = data["set_name"]
+    @set_code     = data["set_code"]
+    @block_name   = data["block_name"]
+    @block_code   = data["block_code"]
+    @border       = data["border"]
+    @release_date = data["releaseDate"]
   end
 
   include Comparable
