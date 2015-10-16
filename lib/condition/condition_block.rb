7319c16b875e25896f538ba558fe4232644506c3
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-16 13:15:28 +0100

    No need for dynamic programming here, straightforward precomputation is fine.

--- lib/condition/condition_block.rb
+++ lib/condition/condition_block.rb
@@ -7,19 +7,30 @@ class ConditionBlock < Condition
   # "in" is code for "Invasion", don't substring match "Innistrad" etc.
   # "Mirrodin" is name for "Mirrodin", don't substring match "Scars of Mirrodin"
   def search(db)
-    block_match = Hash.new do |ht, set|
-      ht[set] = if !set.block_code or !set.block_name
-        false
-      elsif db.blocks.include?(@block)
-        set.block_code == @block or normalize_name(set.block_name) == @block
-      else
-        normalize_name(set.block_name).include?(@block)
-      end
-    end
-    Set.new(db.printings.select{|card| block_match[card.set]})
+    sets = matching_sets(db)
+    Set.new(db.printings.select{|card| sets.include?(card.set_code) })
   end
 
   def to_s
     "b:#{maybe_quote(@block)}"
+  end
+
+  private
+
+  def matching_sets(db)
+    sets = Set[]
+    db.sets.each do |set_code, set|
+      next unless set.block_code and set.block_name
+      if db.blocks.include?(@block)
+        if set.block_code == @block or normalize_name(set.block_name) == @block
+          sets << set_code
+        end
+      else
+        if normalize_name(set.block_name).include?(@block)
+          sets << set_code
+        end
+      end
+    end
+    sets
   end
 end
--- lib/condition/condition_edition.rb
+++ lib/condition/condition_edition.rb
@@ -7,17 +7,29 @@ class ConditionEdition < Condition
   # "in" is code for "Invasion", don't substring match "Innistrad" etc.
   # "Mirrodin" is name for "Mirrodin", don't substring match "Scars of Mirrodin"
   def search(db)
-    edition_match = Hash.new do |ht, set|
-      ht[set] = if db.sets[@edition]
-        set.set_code == @edition or normalize_name(set.set_name) == @edition
-      else
-        normalize_name(set.set_name).include?(@edition)
-      end
-    end
-    Set.new(db.printings.select{|card| edition_match[card.set]})
+    sets = matching_sets(db)
+    Set.new(db.printings.select{|card| sets.include?(card.set_code) })
   end
 
   def to_s
     "e:#{maybe_quote(@edition)}"
+  end
+
+  private
+
+  def matching_sets(db)
+    sets = Set[]
+    db.sets.each do |set_code, set|
+      if db.sets[@edition]
+        if set_code == @edition or normalize_name(set.set_name) == @edition
+          sets << set_code
+        end
+      else
+        if normalize_name(set.set_name).include?(@edition)
+          sets << set_code
+        end
+      end
+    end
+    sets
   end
 end
