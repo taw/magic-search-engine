389867eefd7d8fbcaf8885e16e4c4cd52c6b7718
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-16 13:00:15 +0100

    Remove some hacks

--- lib/card_set.rb
+++ lib/card_set.rb
@@ -1,13 +1,12 @@
 class CardSet
   attr_reader :set_name, :set_code, :block_name, :block_code, :border, :release_date
-  def initialize(data, db)
+  def initialize(data)
     @set_name     = data["set_name"]
     @set_code     = data["set_code"]
     @block_name   = data["block_name"]
     @block_code   = data["block_code"] && data["block_code"].downcase
     @border       = data["border"]
     @release_date = data["release_date"] && Date.parse(data["release_date"])
-    @db = db
   end
 
   include Comparable
@@ -15,34 +14,7 @@ class CardSet
     set_code <=> other.set_code
   end
 
-  # For both of these:
-  # "in" is code for "Invasion", don't substring match "Innistrad" etc.
-  # "Mirrodin" is name for "Mirrodin", don't substring match "Scars of Mirrodin"
-  def match_block?(query)
-    return false unless @block_code and @block_name
-    if @db.blocks.include?(query)
-      @block_code == query.downcase or normalize_text(@block_name) == query
-    else
-      normalize_name(@block_name).include?(normalize_name(query))
-    end
-  end
-
-  def match_set?(query)
-    query = normalize_text(query)
-    if @db.sets[query]
-      @set_code == query.downcase or normalize_text(@set_name) == query
-    else
-      normalize_name(@set_name).include?(normalize_name(query))
-    end
-  end
-
-  private
-
-  def normalize_text(text)
-    text.downcase.gsub(/[Ææ]/, "ae").tr("Äàáâäèéêíõöúûü’\u2212", "Aaaaaeeeioouuu'-").strip
-  end
-
-  def normalize_name(name)
-    normalize_text(name).split.join(" ")
+  def hash
+    @set_code.hash
   end
 end
--- lib/condition/condition_edition.rb
+++ lib/condition/condition_edition.rb
@@ -1,14 +1,20 @@
 class ConditionEdition < Condition
   def initialize(edition)
-    @edition = normalize_text(edition)
-    # FIXME: This is rather silly
-    @edition_cache = Hash.new do |ht, set|
-      ht[set] = set.match_set?(@edition)
-    end
+    @edition = normalize_name(edition)
   end
 
-  def match?(card)
-    @edition_cache[card.set]
+  # For sets and blocks:
+  # "in" is code for "Invasion", don't substring match "Innistrad" etc.
+  # "Mirrodin" is name for "Mirrodin", don't substring match "Scars of Mirrodin"
+  def search(db)
+    edition_match = Hash.new do |ht, set|
+      ht[set] = if db.sets[@edition]
+        set.set_code == @edition or normalize_name(set.set_name) == @edition
+      else
+        normalize_name(set.set_name).include?(@edition)
+      end
+    end
+    Set.new(db.printings.select{|card| edition_match[card.set]})
   end
 
   def to_s
