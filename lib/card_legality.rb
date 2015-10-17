6d9b06d07d2f2b2f4b11eefe4c929a14efbb581d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-17 02:41:28 +0100

    Refactoring card legality logic to be more performant
    It's still way too slow and error-prone and still can't do rotating formats

--- lib/card_legality.rb
+++ lib/card_legality.rb
@@ -135,83 +135,116 @@ BanLists = {
   "Treasure Cruise"=>{"legacy"=>"banned", "modern"=>"banned", "vintage"=>"restricted"}
 }
 
+SetLegality = {
+  "ia" => ["ice age block"],
+  "ai" => ["ice age block"],
+  "cs" => ["ice age block", "modern"],
+  "mr" => ["mirage block"],
+  "vi" => ["mirage block"],
+  "wl" => ["mirage block"],
+  "tp" => ["tempest block"],
+  "sh" => ["tempest block"],
+  "ex" => ["tempest block"],
+  "us" => ["urza block"],
+  "ul" => ["urza block"],
+  "ud" => ["urza block"],
+  "mm" => ["masques block"],
+  "ne" => ["masques block"],
+  "pr" => ["masques block"],
+  "in" => ["invasion block"],
+  "ps" => ["invasion block"],
+  "ap" => ["invasion block"],
+  "od" => ["odyssey block"],
+  "tr" => ["odyssey block"],
+  "ju" => ["odyssey block"],
+  "on" => ["onslaught block"],
+  "le" => ["onslaught block"],
+  "sc" => ["onslaught block"],
+  "mi" => ["modern", "mirrodin block"],
+  "ds" => ["modern", "mirrodin block"],
+  "5dn" => ["modern", "mirrodin block"],
+  "chk" => ["modern", "kamigawa block"],
+  "bok" => ["modern", "kamigawa block"],
+  "sok" => ["modern", "kamigawa block"],
+  "rav" => ["modern", "ravnica block"],
+  "gp" => ["modern", "ravnica block"],
+  "di" => ["modern", "ravnica block"],
+  "ts" => ["modern", "time spiral block"],
+  "tsts" => ["modern", "time spiral block"],
+  "pc" => ["modern", "time spiral block"],
+  "fut" => ["modern", "time spiral block"],
+  "lw" => ["modern", "lorwyn-shadowmoor block"],
+  "mt" => ["modern", "lorwyn-shadowmoor block"],
+  "shm" => ["modern", "lorwyn-shadowmoor block"],
+  "eve" => ["modern", "lorwyn-shadowmoor block"],
+  "ala" => ["modern", "shards of alara block"],
+  "cfx" => ["modern", "shards of alara block"],
+  "arb" => ["modern", "shards of alara block"],
+  "zen" => ["modern", "zendikar block"],
+  "wwk" => ["modern", "zendikar block"],
+  "roe" => ["modern", "zendikar block"],
+  "som" => ["modern", "scars of mirrodin block"],
+  "mbs" => ["modern", "scars of mirrodin block"],
+  "nph" => ["modern", "scars of mirrodin block"],
+  "isd" => ["modern", "innistrad block"],
+  "dka" => ["modern", "innistrad block"],
+  "avr" => ["modern", "innistrad block"],
+  "rtr" => ["modern", "return to ravnica block"],
+  "gtc" => ["modern", "return to ravnica block"],
+  "dgm" => ["modern", "return to ravnica block"],
+  "ths" => ["modern", "theros block"],
+  "bng" => ["modern", "theros block"],
+  "jou" => ["modern", "theros block"],
+  "ktk" => ["standard", "modern", "tarkir block"],
+  "frf" => ["standard", "modern", "tarkir block"],
+  "dtk" => ["standard", "modern", "tarkir block"],
+  "bfz" => ["standard", "modern"],
+  "8e" => ["modern"],
+  "9e" => ["modern"],
+  "10e" => ["modern"],
+  "m10" => ["modern"],
+  "m11" => ["modern"],
+  "m12" => ["modern"],
+  "m13" => ["modern"],
+  "m14" => ["modern"],
+  "m15" => ["modern"],
+  "ori" => ["standard", "modern"],
+}
+
 class CardLegality
   def initialize(name, sets, layout, types)
     @card_name = name
-    @sets = sets
-    @layout = layout
-    @types = types
+    @unsets = sets.include?("uh") || sets.include?("ug")
+    @cns = sets.include?("cns")
+    @sets = sets - %W[arena uqc rep hho uh ug cns] # Ignore un and weirdo printings
+    @extra_layout = ["vanguard", "token", "plane", "scheme", "phenomenon"].include?(layout)
+    @conspiracy = types.include?("conspiracy")
+    @cache = {}
   end
 
-  def legality
-    return {} if ["vanguard", "token", "plane", "scheme", "phenomenon"].include?(@layout)
-    return {"commander"=>"banned", "legacy"=>"banned", "vintage"=>"banned"} if @types.include?("conspiracy")
+  # This is awful in that
+  # we use #all_legalities for sake of indexer validation that legalities are working
+  # then we use #legility for actual checks for performance reasons,
+  # and we just hope they show same stuff
+  # Moving some of that code to constructor makes it a bit less risky, but it's still silly
+  def all_legalities
+    return {} if @extra_layout
+    return {"commander"=>"banned", "legacy"=>"banned", "vintage"=>"banned"} if @conspiracy
 
-    formats = Set[]
+    if @sets.empty?
+      formats = Set[]
+    else
+      formats = Set["commander", "legacy", "vintage"]
+    end
+    formats << "un-sets" if @unsets
+    formats << "legacy" << "vintage" if @cns
     @sets.each do |set|
-      case set
-      when "arena", "uqc", "rep", "hho"
-        # Not real printings, presumably
-        next
-      when "uh", "ug"
-        formats << "un-sets"
-        next
-      when "ia", "ai"
-        formats << "ice age block"
-      when "cs"
-        formats << "ice age block" << "modern"
-      when "mr", "vi", "wl"
-        formats << "mirage block"
-      when "tp", "sh", "ex"
-        formats << "tempest block"
-      when "tp", "sh", "ex"
-        formats << "tempest block"
-      when "us", "ul", "ud"
-        formats << "urza block"
-      when "mm", "ne", "pr"
-        formats << "masques block"
-      when "in", "ps", "ap"
-        formats << "invasion block"
-      when "od", "tr", "ju"
-        formats << "odyssey block"
-      when "on", "le", "sc"
-        formats << "onslaught block"
-      when "mi", "ds", "5dn"
-        formats << "modern" << "mirrodin block"
-      when "chk", "bok", "sok"
-        formats << "modern" << "kamigawa block"
-      when "rav", "gp", "di"
-        formats << "modern" << "ravnica block"
-      when "ts", "tsts", "pc", "fut"
-        formats << "modern" << "time spiral block"
-      when "Lorwyn", "lw", "mt", "shm", "eve"
-        formats << "modern" << "lorwyn-shadowmoor block"
-      when "ala", "cfx", "arb"
-        formats << "modern" << "shards of alara block"
-      when "zen", "wwk", "roe"
-        formats << "modern" << "zendikar block"
-      when "som", "mbs", "nph"
-        formats << "modern" << "scars of mirrodin block"
-      when "isd", "dka", "avr"
-        formats << "modern" << "innistrad block"
-      when "rtr", "gtc", "dgm"
-        formats << "modern" << "return to ravnica block"
-      when "ths", "bng", "jou"
-        formats << "modern" << "theros block"
-      when "ktk", "frf", "dtk"
-        formats << "standard" << "modern" << "tarkir block"
-      when "bfz"
-        formats << "standard" << "modern" # << "battle for zendikar block"
-      when "8e", "9e", "10e", "m10", "m11", "m12", "m13", "m14", "m15"
-        formats << "modern"
-      when "ori"
-        formats << "standard" << "modern"
-      when "cns"
-        # No easy test for draft cards
-        formats << "legacy" << "vintage"
-        next
+      # Not real printings, presumably
+      if SetLegality[set]
+        SetLegality[set].each do |fmt|
+          formats << fmt
+        end
       end
-      formats << "commander" << "legacy" << "vintage"
     end
     leg = {}
     formats.each do |format|
@@ -219,5 +252,34 @@ class CardLegality
     end
     leg.merge!(BanLists[@card_name]) if BanLists[@card_name]
     leg
+  end
+
+  def legality(format)
+    unless @cache.has_key?(format)
+      @cache[format] = begin
+        eternal_format = ["commander", "legacy", "vintage"].include?(format)
+        if format == "un-sets"
+          @unsets
+        elsif @extra_layout
+          nil
+        elsif @conspiracy
+          eternal_format ? "banned" : nil
+        else
+          legality = nil
+          if eternal_format
+            legality = "legal" if !@sets.empty?
+            legality = "legal" if @cns and format != "commander"
+          else
+            legality = "legal" if @sets.any?{|set| SetLegality.fetch(set, []).include?(format) }
+          end
+          if legality
+            BanLists.fetch(@card_name, {}).fetch(format, legality)
+          else
+            nil
+          end
+        end
+      end
+    end
+    @cache[format]
   end
 end
