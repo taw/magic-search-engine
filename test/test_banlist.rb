3a044daedee5e53729ba4a920b08cac93c80ea66
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-10-18 10:43:52 +0100

    Spec for banlist changes 2000+, ones that fit the format at least

--- test/test_banlist.rb
+++ test/test_banlist.rb
@@ -15,9 +15,6 @@ class BanlistTest < Minitest::Test
   end
 
   def assert_banlist_status(set, format, expected_legality, card_name)
-    # TEMPORARY HACK
-    return unless format == "modern" or format == "innistrad block"
-
     set_date = @db.sets[set].release_date
     card = @db.cards[card_name]
     actual_legality = @ban_list.legality(format, card_name, set_date) || "legal"
@@ -160,5 +157,197 @@ class BanlistTest < Minitest::Test
       "Umezawa's Jitte",
       "Valakut, the Molten Pinnacle",
       "Vault of Whispers"
+  end
+
+  def test_banlist_2010
+    assert_banlist_changes "June 2010",
+      "extended banned", "Sword of the Meek",
+      "extended banned", "Hypergenesis",
+      "legacy banned", "Mystical Tutor",
+      "legacy unbanned", "Grim Monolith",
+      "legacy unbanned", "Illusionary Mask"
+    assert_banlist_changes "September 2010",
+      "vintage unrestricted", "Frantic Search",
+      "vintage unrestricted", "Gush"
+    assert_banlist_changes "December 2010",
+      "legacy banned", "Survival of the Fittest",
+      "legacy unbanned", "Time Spiral"
+  end
+
+  def test_banlist_2009
+    assert_banlist_changes "June 2009",
+      "vintage restricted", "Thirst for Knowledge",
+      "vintage unrestricted", "Crop Rotation",
+      "vintage unrestricted", "Enlightened Tutor",
+      "vintage unrestricted", "Entomb",
+      "vintage unrestricted", "Grim Monolith"
+    assert_banlist_changes "September 2009",
+      "legacy unbanned", "Dream Halls",
+      "legacy unbanned", "Entomb",
+      "legacy unbanned", "Metalworker"
+  end
+
+  def test_banlist_2008
+    assert_banlist_changes "June 2008",
+      "vintage restricted", "Brainstorm",
+      "vintage restricted", "Flash",
+      "vintage restricted", "Gush",
+      "vintage restricted", "Merchant Scroll",
+      "vintage restricted", "Ponder"
+    assert_banlist_changes "September 2008",
+      "extended banned", "Sensei's Divining Top",
+      "legacy banned", "Time Vault",
+      "vintage restricted", "Time Vault",
+      "vintage unrestricted", "Chrome Mox",
+      "vintage unrestricted", "Dream Halls",
+      "vintage unrestricted", "Mox Diamond",
+      "vintage unrestricted", "Personal Tutor",
+      "vintage unrestricted", "Time Spiral"
+  end
+
+  def test_banlist_2007
+    assert_banlist_changes "June 2007",
+      "vintage restricted", "Gifts Ungiven",
+      "vintage unrestricted", "Voltaic Key",
+      "vintage unrestricted", "Black Vise",
+      "vintage unrestricted", "Mind Twist",
+      "vintage unrestricted", "Gush",
+      "legacy banned", "Flash",
+      "legacy unbanned", "Mind Over Matter",
+      "legacy unbanned", "Replenish"
+
+    assert_banlist_changes "September 2007",
+      "vintage banned", "Shahrazad",
+      "legacy banned", "Shahrazad"
+  end
+
+  def test_banlist_2006
+    assert_banlist_changes "March 2006",
+      "mirrodin block banned", "AEther Vial",
+      "mirrodin block banned", "Ancient Den",
+      "mirrodin block banned", "Arcbound Ravager",
+      "mirrodin block banned", "Darksteel Citadel",
+      "mirrodin block banned", "Disciple of the Vault",
+      "mirrodin block banned", "Great Furnace",
+      "mirrodin block banned", "Seat of the Synod",
+      "mirrodin block banned", "Tree of Tales",
+      "mirrodin block banned", "Vault of Whispers"
+  end
+
+  def test_banlist_2005
+    assert_banlist_changes "March 2005",
+      "standard banned", "Arcbound Ravager",
+      "standard banned", "Disciple of the Vault",
+      "standard banned", "Darksteel Citadel",
+      "standard banned", "Ancient Den",
+      "standard banned", "Great Furnace",
+      "standard banned", "Seat of the Synod",
+      "standard banned", "Tree of Tales",
+      "standard banned", "Vault of Whispers",
+      "vintage restricted", "Trinisphere"
+
+    # Starter Level sets Starter 1999, Starter 2000, Portal, Portal Second Age, and Portal Three Kingdoms become legal in Legacy and Vintage in October.
+
+    assert_banlist_changes "September 2005",
+      "extended banned", "Aether Vial",
+      "extended banned", "Disciple of the Vault",
+      "legacy banned", "Imperial Seal",
+      "vintage restricted", "Imperial Seal",
+      "vintage restricted", "Personal Tutor",
+      "vintage unrestricted", "Mind Over Matter",
+      "two-headed giant banned", "Erayo, Soratami Ascendant"
+  end
+
+  def test_banlist_2004
+    assert_banlist_changes "June 2004",
+      "standard banned", "Skullclamp",
+      "mirrodin block banned", "Skullclamp"
+
+    assert_banlist_changes "September 2004",
+      "extended banned", "Metalworker",
+      "extended banned", "Skullclamp",
+      "vintage unrestricted", "Braingeyser",
+      "vintage unrestricted", "Doomsday",
+      "vintage unrestricted", "Earthcraft",
+      "vintage unrestricted", "Fork"
+
+    # assert_banlist_changes "September 2004",
+      # "Legacy becomes independent of Vintage"
+
+    assert_banlist_status "December 2004",
+      "vintage unrestricted", "Stroke of Genius"
+  end
+
+  def test_banlist_2003
+    assert_banlist_changes "March 2003",
+      "vintage unrestricted", "Berserk",
+      "vintage unrestricted", "Hurkyl's Recall",
+      "vintage unrestricted", "Recall" ,
+      "vintage restricted", "Earthcraft",
+      "vintage restricted", "Entomb",
+      "legacy unbanned", "Berserk",
+      "legacy unbanned", "Hurkyl's Recall",
+      "legacy unbanned", "Recall",
+      "legacy banned", "Earthcraft",
+      "legacy banned", "Entomb"
+
+    assert_banlist_changes "June 2003",
+      "vintage restricted", "Gush",
+      "vintage restricted", "Mind's Desire",
+      "legacy banned", "Gush",
+      "legacy banned", "Mind's Desire"
+
+    assert_banlist_changes "September 2003",
+      "extended banned", "Goblin Lackey",
+      "extended banned", "Entomb",
+      "extended banned", "Frantic Search"
+
+    assert_banlist_changes "December 2003",
+      "extended banned", "Goblin Recruiter",
+      "extended banned", "Grim Monolith",
+      "extended banned", "Tinker",
+      "extended banned", "Hermit Druid",
+      "extended banned", "Ancient Tomb",
+      "extended banned", "Oath of Druids",
+      "vintage restricted", "Burning Wish",
+      "vintage restricted", "Chrome Mox",
+      "vintage restricted", "Lion's Eye Diamond",
+      "legacy banned", "Burning Wish",
+      "legacy banned", "Chrome Mox",
+      "legacy banned", "Lion's Eye Diamond"
+  end
+
+  def test_banlist_2002
+    # No changes whole year
+  end
+
+  def test_banlist_2001
+    assert_banlist_changes "March 2001",
+      "extended banned", "Necropotence",
+      "extended banned", "Replenish",
+      "extended banned", "Survival of the Fittest",
+      "extended banned", "Demonic Consultation"
+
+    assert_banlist_changes "December 2001",
+      "vintage restricted", "Fact or Fiction",
+      "legacy banned", "Fact or Fiction"
+  end
+
+  def test_banlist_2000
+    assert_banlist_changes "March 2000",
+      "extended banned", "Dark Ritual",
+      "extended banned", "Mana Vault"
+
+    assert_banlist_changes "June 2000",
+      "masques block banned", "Lin Sivvi, Defiant Hero",
+      "masques block banned", "Rishadan Port"
+
+    assert_banlist_changes "September 2000",
+      "vintage banned-to-restricte", "Channel",
+      "vintage banned-to-restricte", "Mind Twist",
+      "vintage restricted", "Demonic Consultation",
+      "vintage restricted", "Necropotence",
+      "legacy banned", "Demonic Consultation",
+      "legacy banned", "Necropotence"
   end
 end
