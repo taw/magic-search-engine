d4ba0d045f54beddabd9c269044f02fe15c3e09a
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-02 01:47:23 +0000

    List matching / non-matching printings. UI is absolutely awful for basic lands, but mostly OK otherwise.

--- frontend/app/controllers/card_controller.rb
+++ frontend/app/controllers/card_controller.rb
@@ -8,17 +8,27 @@ class CardController < ApplicationController
     render_404 unless @card
   end
 
+  # Logic tested in CLIFrontend, probably should be moved to database
+  # as this untested copypasta is nasty
   def index
     @search = params[:q] || ""
     page = [1, params[:page].to_i].max
 
     query = Query.new(@search)
     if @search.present?
-      @cards = $CardDatabase.search(query).uniq(&:name)
+      results = $CardDatabase.search(query)
     else
-      @cards = []
+      results = []
     end
 
-    @cards = @cards.paginate(page: page, per_page: 25)
+    @cards = choose_best_printing(results).paginate(page: page, per_page: 25)
+  end
+
+  private
+
+  def choose_best_printing(results)
+    results.group_by(&:name).map{|name, printings|
+      [printings[0], printings]
+    }
   end
 end
