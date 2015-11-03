a78275c47ea1a6db93b727286f816e91025b48f3
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-03 18:36:18 +0000

    Some very minimal tests for Rails side

--- frontend/test/controllers/help_controller_test.rb
+++ frontend/test/controllers/help_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class HelpControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "smoke test" do
+    get "syntax"
+    assert_response 200
+    assert_select %Q[h3:contains("Search Help")]
+  end
 end
