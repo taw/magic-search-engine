72dc17bd35359e2cfb39951628308d829f73639c
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-03 18:36:18 +0000

    Some very minimal tests for Rails side

--- frontend/test/controllers/card_controller_test.rb
+++ frontend/test/controllers/card_controller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class CardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # show
+  test "show card" do
+    get "show", set: "nph", id: "1"
+    assert_response 200
+    assert_select %Q[.card:contains("Karn Liberated")]
+  end
+
+  test "bad set" do
+    get "show", set: "lolwtf", id: "1"
+    assert_response 404
+  end
+
+  test "bad collector number" do
+    get "show", set: "nph", id: "1000"
+    assert_response 404
+  end
+
+  # search
+    test "search nothing" do
+    get "index"
+    assert_response 200
+    assert_select ".card", 0
+  end
+
+  test "search all" do
+    get "index", q: "sort:new"
+    assert_response 200
+    assert_select ".card", 25
+  end
+
+  test "search something" do
+    get "index", q: "Karn Liberated"
+    assert_response 200
+    assert_select %Q[.card:contains("Karn Liberated")]
+  end
 end
+
--- frontend/test/controllers/set_controller_test.rb
+++ frontend/test/controllers/set_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SetControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "list of sets" do
+    get "index"
+    assert_response 200
+    assert_select %Q[a:contains("Magic 2010")]
+  end
+
+  test "actual set" do
+    get "show", id: "nph"
+    assert_response 200
+    assert_select %Q[h3:contains("New Phyrexia")]
+    assert_select %Q[a:contains("Karn Liberated")]
+  end
+
+  test "fake set" do
+    get "show", id: "lolwtf"
+    assert_response 404
+  end
 end
