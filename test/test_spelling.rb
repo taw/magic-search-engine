af352072b9678e8100ae60cafe9d625244d9eb9f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-11-04 01:16:44 +0000

    Max spelling correction count reduced to 0 (1-2 chars), 1 (3-4 chars), 2 (5+ chars)

--- test/test_spelling.rb
+++ test/test_spelling.rb
@@ -16,6 +16,18 @@ class SpellingTest < Minitest::Test
     assert_spelling_suggestions "ajuni's", "ajani"
   end
 
+  def test_spelling_short_words
+    # 1-2 letters - 0 corrections
+    assert_spelling_suggestions "7"
+    assert_spelling_suggestions "77"
+    # 3-4 letters - 1 correction
+    assert_spelling_suggestions "mux", "lux", "mox", "mul"
+    assert_spelling_suggestions "xxx"
+    # size applied after unicode normalization
+    assert_spelling_suggestions "aethr", "aether"
+    assert_spelling_suggestions "æthr", "aether"
+  end
+
   def assert_spelling_suggestions(word, *expected_suggestions)
     actual_suggestions = @db.suggest_spelling(word)
     assert_equal expected_suggestions, actual_suggestions, "Spelling suggestions for #{word}"
