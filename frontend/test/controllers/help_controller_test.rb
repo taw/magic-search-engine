2519ba6af3e685c6d92e77d9d9c8ef9587d7366c
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-12-02 04:45:50 +0000

    Put page title before site title

--- frontend/test/controllers/help_controller_test.rb
+++ frontend/test/controllers/help_controller_test.rb
@@ -5,20 +5,20 @@ class HelpControllerTest < ActionController::TestCase
     get "syntax"
     assert_response 200
     assert_select %Q[h3:contains("Search Help")]
-    assert_equal "mtg.wtf - Syntax", html_document.title
+    assert_equal "Syntax - mtg.wtf", html_document.title
   end
 
   test "comprehensive rules" do
     get "rules"
     assert_response 200
     assert_select %Q[*:contains("Magic: The Gathering Comprehensive Rules")]
-    assert_equal "mtg.wtf - Magic: The Gathering Comprehensive Rules", html_document.title
+    assert_equal "Magic: The Gathering Comprehensive Rules - mtg.wtf", html_document.title
   end
 
   test "contact" do
     get "contact"
     assert_response 200
     assert_select %Q[div:contains("You can contact me by")]
-    assert_equal "mtg.wtf - Contact", html_document.title
+    assert_equal "Contact - mtg.wtf", html_document.title
   end
 end
