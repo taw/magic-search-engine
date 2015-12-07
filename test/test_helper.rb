21c304a2f203e2e0459f2d4903de2b5ffcf70569
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2015-12-07 14:58:45 +0000

    Starting work on interactive query builder (or "advanced search")

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require_relative "../lib/card_database"
 require_relative "../lib/cli_frontend"
+require_relative "../lib/interactive_query_builder"
 
 class Minitest::Test
   # minitest is shitty and doesn't report test results in ordered way
