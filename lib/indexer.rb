c5c2174cafa3094e47ec2efef00998defb51c4fc
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-07-18 01:30:22 +0100

    Move card renumbering logic to Indexer::CardSet

--- lib/indexer.rb
+++ lib/indexer.rb
@@ -184,9 +184,10 @@ class Indexer
 
     @data.each do |set_code, set_data|
       set_code = @sets_code_translator[set_code]
-      sets[set_code] = Indexer::CardSet.new(set_code, set_data).to_json
+      set = Indexer::CardSet.new(set_code, set_data)
+      sets[set_code] = set.to_json
 
-      ensure_set_has_card_numbers!(set_code, set_data)
+      set.ensure_set_has_card_numbers!
 
       set_data["cards"].each do |card_data|
         name = card_data["name"]
@@ -324,93 +325,6 @@ class Indexer
 
   def normalize_name(name)
     name.gsub("Æ", "Ae").tr("Äàáâäèéêíõöúûü", "Aaaaaeeeioouuu")
-  end
-
-  # They really only have two goals:
-  # * be unique
-  # * allow linking to magiccards.info for debugging
-  #
-  # For uniqueness sake we could just allocate them ourselves easily
-  # But to make them compatible instead we use mci's numbers
-  #
-  # For cards with multiple printings it's not obvious how to get them arranged so pics match,
-  # but it's not a huge deal
-  #
-  # By the way ordering by multiverseid would probably be more sensible
-  # (alpha starts with Ankh of Mishra, not Animate Dead), but compatibilty etc.
-
-  def ensure_set_has_card_numbers!(set_code, set_data)
-    cards = set_data["cards"]
-    numbers = cards.map{|c| c["number"]}
-
-    if numbers.compact.empty?
-      use_mci_numbers!(set_code, set_data)
-    end
-
-    case set_code
-    when "van"
-      set_data["cards"].sort_by{|c| c["multiverseid"]}.each_with_index{|c,i| c["number"] = "#{i+1}"}
-    when "pch", "arc", "pc2"
-      set_data["cards"].each do |card|
-        unless (card["types"] & ["Plane", "Phenomenon", "Scheme"]).empty?
-          card["number"] = (1000 + card["number"].to_i).to_s
-        end
-      end
-    when "bfz", "ogw"
-      # No idea if this is correct
-      basic_land_cards = set_data["cards"].select{|c| (c["supertypes"]||[]) .include?("Basic") }
-      basic_land_cards = basic_land_cards.sort_by{|c| [c["number"], c["multiverseid"]]}
-      basic_land_cards.each_slice(2) do |a,b|
-        raise unless a["number"] == b["number"]
-        b["number"] += "A"
-      end
-    when "rqs", "me4", "clash"
-      # Just brute force, investigate later wtf?
-      set_data["cards"].sort_by{|c| c["multiverseid"]}.each_with_index{|c,i| c["number"] = "#{i+1}"}
-    when "st2k"
-      # Just brute force, investigate later wtf?
-      set_data["cards"].each_with_index{|c,i| c["number"] = "#{i+1}"}
-    end
-
-    numbers = cards.map{|c| c["number"]}
-    if numbers.compact.size == 0
-      warn "Set #{set_code} #{set_data["name"]} has NO numbers"
-    elsif numbers.compact.size != numbers.size
-      warn "Set #{set_code} #{set_data["name"]} has cards without numbers"
-    end
-    if numbers.compact.size != numbers.compact.uniq.size
-      warn "Set #{set_code} #{set_data["name"]} has DUPLICATE numbers"
-    end
-  end
-
-  # Assume that if two Forests are 100 and 101
-  # then Forest with lower multiverse id gets 100
-  # No idea if that's correct
-  def use_mci_numbers!(set_code, set_data)
-    path = Pathname(__dir__) + "../data/collector_numbers/#{set_code}.txt"
-    return unless path.exist?
-    mci_numbers = path.readlines.map{|line|
-      number, name = line.chomp.split("\t", 2)
-      [number, name.downcase]
-    }.group_by(&:last).map_values{|x| x.map(&:first)}
-
-    cards = set_data["cards"]
-
-    if set_code == "ced" or set_code == "cedi"
-      # Not on Gatherer
-      cards.each do |card|
-        name = card["name"]
-        card["number"] = mci_numbers[name.downcase].shift
-      end
-    else
-      mvids = cards.map{|c| [c["name"], c["multiverseid"]]}.group_by(&:first).map_values{|x| x.map(&:last).sort}
-      cards.each do |card|
-        name = card["name"]
-        rel_idx = mvids[name].index(card["multiverseid"])
-        raise unless mci_numbers[name.downcase]
-        card["number"] = mci_numbers[name.downcase][rel_idx]
-      end
-    end
   end
 
   # FIXME: This really shouldn't be here
