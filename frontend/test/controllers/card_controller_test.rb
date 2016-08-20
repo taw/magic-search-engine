2e36c4f65e64a8c7f93471801056a5c06862b237
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-08-20 19:56:32 +0100

    Updating tests to match bootstrap

--- frontend/test/controllers/card_controller_test.rb
+++ frontend/test/controllers/card_controller_test.rb
@@ -5,7 +5,7 @@ class CardControllerTest < ActionController::TestCase
   test "show card" do
     get "show", set: "nph", id: "1"
     assert_response 200
-    assert_select %Q[.card:contains("Karn Liberated")]
+    assert_select %Q[.cardinfo:contains("Karn Liberated")]
     assert_equal "Karn Liberated - mtg.wtf", html_document.title
   end
 
@@ -23,30 +23,31 @@ class CardControllerTest < ActionController::TestCase
     test "search nothing" do
     get "index"
     assert_response 200
-    assert_select ".card", 0
-    assert_select ".zero_results", 0
+    assert_select ".cardinfo", 0
+    assert_select ".results_summary", 0
     assert_equal "mtg.wtf", html_document.title
   end
 
   test "search all" do
     get "index", q: "sort:new"
     assert_response 200
-    assert_select ".card", 25
+    assert_select ".cardinfo", 25
     assert_equal "sort:new - mtg.wtf", html_document.title
   end
 
   test "nothing found" do
     get "index", q: "optimus prime"
     assert_response 200
-    assert_select ".card", 0
-    assert_select ".zero_results"
+    assert_select ".cardinfo", 0
+    assert_select %Q[.results_summary:contains("No cards found")]
     assert_equal "optimus prime - mtg.wtf", html_document.title
   end
 
   test "search something" do
     get "index", q: "Karn Liberated"
     assert_response 200
-    assert_select %Q[.card:contains("Karn Liberated")]
+    assert_select %Q[.cardinfo:contains("Karn Liberated")]
+    assert_select %Q[.results_summary:contains("1 card found")]
     assert_equal "Karn Liberated - mtg.wtf", html_document.title
   end
 end
