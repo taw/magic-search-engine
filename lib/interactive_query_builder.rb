fd056d826ce6bb368a00870313704b7327a50206
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-08-23 17:47:50 +0100

    Expanded InteractiveQueryBuilder to handle rarity, set, block, and watermark

--- lib/interactive_query_builder.rb
+++ lib/interactive_query_builder.rb
@@ -5,36 +5,62 @@ class InteractiveQueryBuilder
 
   def query
     conds = []
+    set_block_conds = []
     @args.each do |cond, keys|
       case cond.to_s
       when "name"
-        parse_fragments(keys).each do |frag|
-          conds << maybe_quote(frag)
-        end
+        conds.push *text_and_condition("", keys)
       when "type"
-        parse_fragments(keys).each do |frag|
-          conds << "t:#{maybe_quote(frag)}"
-        end
+        conds.push *text_and_condition("t:", keys)
       when "oracle"
-        parse_fragments(keys).each do |frag|
-          conds << "o:#{maybe_quote(frag)}"
-        end
+        conds.push *text_and_condition("o:", keys)
       when "flavor"
-        parse_fragments(keys).each do |frag|
-          conds << "ft:#{maybe_quote(frag)}"
-        end
+        conds.push *text_and_condition("ft:", keys)
       when "artist"
-        parse_fragments(keys).each do |frag|
-          conds << "a:#{maybe_quote(frag)}"
-        end
+        conds.push *text_and_condition("a:", keys)
+      when "rarity"
+        conds.push *or_condition(flag_condition("r:", keys))
+      when "set"
+        set_block_conds.push *flag_condition("e:", keys)
+      when "block"
+        set_block_conds.push *flag_condition("b:", keys)
+      when "watermark"
+        conds.push *or_condition(flag_condition("w:", keys))
       else
         raise "Unknown key: #{cond}"
       end
+    end
+
+    unless set_block_conds.empty?
+      conds.push *or_condition(set_block_conds)
     end
     conds.join(" ")
   end
 
   private
+
+  def text_and_condition(prefix, keys)
+    parse_fragments(keys).map do |frag|
+      "#{prefix}#{maybe_quote(frag)}"
+    end
+  end
+
+  def flag_condition(prefix, keys)
+    keys.map do |frag|
+      "#{prefix}#{maybe_quote(frag)}"
+    end
+  end
+
+  def or_condition(conds)
+    case conds.size
+    when 0
+      nil
+    when 1
+      conds[0]
+    else
+      "(#{ conds.join(" OR ") })"
+    end
+  end
 
   def parse_fragments(str)
     str.scan(/"(.*?)"|(\S+)/).flatten.compact
--- test/test_interactive_query_builder.rb
+++ test/test_interactive_query_builder.rb
@@ -35,4 +35,24 @@ class InteractiveQueryBuilderTest < Minitest::Test
     assert_interactive_query 'a:steve a:argyle', artist: "steve argyle"
     assert_interactive_query 'a:"steve argyle"', artist: '"steve argyle"'
   end
+
+  def test_rarity
+    assert_interactive_query "r:uncommon", rarity: ["uncommon"]
+    assert_interactive_query "(r:rare OR r:mythic)", rarity: ["rare", "mythic"]
+  end
+
+  def test_set
+    assert_interactive_query "e:ktk", set: ["ktk"]
+    assert_interactive_query "(e:ktk OR e:m10)", set: ["ktk", "m10"]
+  end
+
+  def test_block
+    assert_interactive_query "b:isd", block: ["isd"]
+    assert_interactive_query "(b:zen OR b:bfz)", block: ["zen", "bfz"]
+  end
+
+  def test_watermark
+    assert_interactive_query "w:boros", watermark: ["boros"]
+    assert_interactive_query "(w:izzet OR w:abzan)", watermark: ["izzet", "abzan"]
+  end
 end
