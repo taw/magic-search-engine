574117d825eb284e8a5dded8e377f2b29a0c1e00
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-08-25 14:38:59 +0100

    Disable partial spam in logs

--- frontend/config/application.rb
+++ frontend/config/application.rb
@@ -27,5 +27,6 @@ module Frontend
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_view.logger = nil
   end
 end
