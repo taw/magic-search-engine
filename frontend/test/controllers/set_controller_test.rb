db6d0df26177b5a78ff25dd2515ae5277f11e8bb
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-08-28 23:12:15 +0100

    Count cards on set screen

--- frontend/test/controllers/set_controller_test.rb
+++ frontend/test/controllers/set_controller_test.rb
@@ -11,6 +11,7 @@ class SetControllerTest < ActionController::TestCase
   test "actual set" do
     get "show", id: "nph"
     assert_response 200
+    assert_select %Q[.results_summary:contains("New Phyrexia contains 175 cards")]
     assert_select %Q[h3:contains("New Phyrexia")]
     assert_select %Q[a:contains("Karn Liberated")]
     assert_equal "New Phyrexia - mtg.wtf", html_document.title
