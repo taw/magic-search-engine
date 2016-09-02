17f6d547493e3fbe32e7cc85cc30677e18e866c1
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-02 23:55:12 +0100

    Improved tests of Set controller

--- frontend/test/controllers/set_controller_test.rb
+++ frontend/test/controllers/set_controller_test.rb
@@ -4,14 +4,16 @@ class SetControllerTest < ActionController::TestCase
   test "list of sets" do
     get "index"
     assert_response 200
-    assert_select %Q[a:contains("Magic 2010")]
+    assert_select %Q[a:contains("Magic 2015 Core Set")]
+    assert_select %Q[li:contains("Magic 2015 Core Set\n(M15, 284 cards)")]
     assert_equal "Sets - mtg.wtf", html_document.title
   end
 
   test "actual set" do
     get "show", id: "nph"
     assert_response 200
-    assert_select %Q[.results_summary:contains("New Phyrexia contains 175 cards")]
+    assert_select %Q[.results_summary:contains("New Phyrexia contains 175 cards.")]
+    assert_select %Q[.results_summary:contains("It is part of Scars of Mirrodin block.")]
     assert_select %Q[h3:contains("New Phyrexia")]
     assert_select %Q[a:contains("Karn Liberated")]
     assert_equal "New Phyrexia - mtg.wtf", html_document.title
