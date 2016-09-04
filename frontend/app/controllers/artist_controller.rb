f8ed903356edb883219f37e6520fe3c9bf4ce91a
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-04 23:59:08 +0100

    Gallery view

--- frontend/app/controllers/artist_controller.rb
+++ frontend/app/controllers/artist_controller.rb
@@ -1,10 +1,10 @@
 class ArtistController < ApplicationController
   def index
     @title = "Artists"
-    @artists = Set[]
+    @artists = Hash.new(0)
     $CardDatabase.sets.each_value do |set|
       set.printings.each do |printing|
-        @artists << printing.artist
+        @artists[printing.artist] += 1
       end
     end
     @artists = @artists.sort
@@ -13,14 +13,17 @@ class ArtistController < ApplicationController
   def show
     id = params[:id]
     @artist = id
-    @cards = []
-    $CardDatabase.sets.each_value do |set|
-      set.printings.each do |printing|
-        @cards << printing if printing.artist == id
+    @cards_by_set = {}
+    sets = $CardDatabase.sets.values.sort_by{|s| [-s.release_date.to_i_sort, s.code]}
+    sets.each do |set|
+      cards = set.printings.select{|printing| printing.artist == id}
+      if cards.present?
+        @cards_by_set[set] ||= cards
       end
     end
-    @cards.sort_by{|cp| [cp.release_date, cp.number.to_i, cp.number]}
-    page = [1, params[:page].to_i].max
-    @cards = @cards.paginate(page: page, per_page: 25)
+    @cards_total = @cards_by_set.values.map(&:size).inject(0, &:+)
+    # @cards.sort_by{|cp| [cp.release_date, cp.number.to_i, cp.number]}
+    # page = [1, params[:page].to_i].max
+    # @cards = @cards.paginate(page: page, per_page: 25)
   end
 end
