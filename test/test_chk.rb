8dedd293cc4f2da2b9349eb026e472d08175f0f8
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-22 12:47:40 +0100

    709.1c test

--- test/test_chk.rb
+++ test/test_chk.rb
@@ -11,4 +11,10 @@ class CardDatabaseCHKTest < Minitest::Test
     assert_search_include "is:flip", "Akki Lavarunner"
     assert_search_exclude "not:flip", "Akki Lavarunner"
   end
+
+  # 709.1c A flip card’s color and mana cost don’t change if the permanent is flipped. Also, any changes to it by external effects will still apply.
+  def test_flip_mana_cost
+    assert_search_include "cmc=4", "Kitsune Mystic", "Autumn-Tail, Kitsune Sage"
+    assert_search_include "mana=3w", "Kitsune Mystic", "Autumn-Tail, Kitsune Sage"
+  end
 end
