88f63ef3ee28094793db9facbd7d6c525fffe713
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-10-10 21:56:29 +0100

    Speeding up ConditionOracle, 191ms to 131ms per request

--- lib/condition/condition_oracle.rb
+++ lib/condition/condition_oracle.rb
@@ -1,10 +1,16 @@
 class ConditionOracle < ConditionSimple
   def initialize(text)
     @text = text
+    @has_cardname = !!(@text =~ /~/)
+    @normalized_text = normalize_text(@text)
   end
 
   def match?(card)
-    normalize_text(card.text).include?(normalize_text(@text.gsub("~", card.name)))
+    if @has_cardname
+      card.text.downcase.include?(@text.gsub("~", normalize_text(card.name)))
+    else
+      card.text.downcase.include?(@normalized_text)
+    end
   end
 
   def to_s
