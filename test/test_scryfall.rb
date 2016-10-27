51e14a886e32d4ee03024d914b34395df942ef32
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-10-27 13:55:18 +0100

    More scryfall tests

--- test/test_scryfall.rb
+++ test/test_scryfall.rb
@@ -57,6 +57,19 @@ class ScryfallTest < Minitest::Test
     assert_count_results "is:meld", 9
   end
 
+  def test_ft_designed
+    # identical results
+    assert_count_results 'ft:"designed" e:m15', 15
+  end
+
+  def test_ft_mishra
+    # results differ due to uncards
+    assert_search_include 'ft:"mishra"',
+      "Battering Ram",
+      "Ankh of Mishra",
+      "Mishra's Toy Workshop"
+  end
+
   def test_creature_type
     # scryfall includes changelings as every creature type
     assert_search_include "t:merfolk t:legendary",
@@ -85,6 +98,93 @@ class ScryfallTest < Minitest::Test
   def test_restricted_vintage
     # Identical results
     assert_count_results "restricted:vintage", 43
+  end
+
+  def test_e_mm2
+    # Identical results
+    assert_count_results "e:mm2", 249
+  end
+
+  def test_b_zen
+    # Identical results
+    assert_count_results "b:zen", 607
+  end
+
+  def test_frame_future
+    assert_search_equal "frame:future", "is:future"
+  end
+
+  def test_m_gg
+    # m: as extra alias for mana: is fine,
+    # but what scryfall does with m: being symbol-level m>= is insanity
+
+    assert_search_equal "m={g}{g}", "mana={g}{g}"
+    assert_search_equal "m:{g}{g}", "mana:{g}{g}"
+    assert_search_equal "m:{g}{g}", "m={g}{g}"
+
+    # scryfall thinks 2gg should match gg
+    assert_search_include "m:{g}{g}", "Bassara Tower Archer"
+    assert_search_exclude "m:{g}{g}", "Abundance"
+
+    # scryfall thinks m:4 should match 4g but not 5, that's just nonsense
+    assert_search_include "m:4", "Solemn Simulacrum"
+    assert_search_exclude "m:4", "Ballista Charger", "Air Servant"
+
+    assert_search_include "m>=4",
+      "Solemn Simulacrum",  # 4
+      "Abhorrent Overlord", # 5bb
+      "Aether Searcher",    # 7
+      "Aethersnatch"        # 4uu
+    assert_search_exclude "m>=4",
+      "Academy Elite"       # 3u
+  end
+
+  def test_m_2ww
+    # same problems as m:{g}{g}
+    assert_search_equal "m:2WW", "cmc=4 c:w m:2ww"
+  end
+
+  def test_is_split
+    # Results differ due to uncards
+    assert_search_include "is:split",
+      "Alive", "Well",
+      "Boom", "Bust",
+      "Naughty", "Nice"
+  end
+
+  def test_exact_fire
+    assert_search_results "!Fire", "Fire"
+  end
+
+  def test_exact_sift_through_sands
+    assert_search_results '!sift through sands', "Sift Through Sands"
+    assert_search_results '!"sift through sands"', "Sift Through Sands"
+  end
+
+
+  def test_is_commander
+    # arguable if banned cards should be included,
+    # but there are multiple commander-like formats
+    # with multiple banlists
+    assert_search_include "is:commander",
+      "Erayo, Soratami Ascendant",
+      "Daretti, Scrap Savant",
+      "Agrus Kos, Wojek Veteran",
+      "Griselbrand",
+      "Bruna, the Fading Light",
+      "Ulrich of the Krallenhorde"
+
+    # Ton of scryfall bugs with cards which are
+    # legendary creatures on non-prmary sides
+    assert_search_exclude "is:commander",
+      "Erayo's Essence",
+      "Autumn-Tail, Kitsune Sage",
+      "Brisela, Voice of Nightmares",
+      "Hanweir, the Writhing Township",
+      "Ormendahl, Profane Prince",
+      "Ulrich, Uncontested Alpha",
+      "Withengar Unbound",
+      "Elbrus, the Binding Blade"
   end
 
   def test_parentheses
