c724c83e79b5f2e733a07c9a6fcd467334f39587
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-11-06 00:43:59 +0000

    Vanguard test

--- spec/fnm_spec.rb
+++ spec/fnm_spec.rb
@@ -4,6 +4,6 @@ describe "FNM" do
   it "year" do
     "year = 2000".should have_result_count 11
     "year < 2003".should have_result_count 30
-    "year > 2004".should equal_result "year >= 2005 "
+    "year > 2004".should equal_search "year >= 2005 "
   end
 end
