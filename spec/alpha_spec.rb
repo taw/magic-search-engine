da1e13e6ad7a3e0b5ed8a8270188dd5d1f192631
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-11-06 00:14:25 +0000

    Migrated Alpha test

---
+++ spec/alpha_spec.rb
@@ -0,0 +1,16 @@
+describe "Alpha" do
+  include_context "db"
+
+  it "legality" do
+    "e:al f:edh"    .should equal_search "e:al f:commander"
+    "e:al legal:edh".should equal_search "e:al legal:commander"
+    "e:al f:legacy" .should equal_search "e:al -banned:legacy"
+    "e:al f:vintage".should equal_search "e:al -banned:vintage"
+    "e:al f:modern" .should equal_search "e:al legal:modern"
+  end
+
+  it "reserved" do
+    "is:reserved" .should include_cards "Black Lotus"
+    "not:reserved".should include_cards "Shivan Dragon"
+  end
+end
