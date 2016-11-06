081d128d4f37f7cd44062ae4e71e586de4436c79
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-11-06 03:20:09 +0000

    Fixing remaining migration issues for reals now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,9 @@ RSpec.configure do |config|
   config.expect_with(:rspec) do |c|
     c.syntax = :should
   end
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true
+  end
 end
 
 RSpec::Matchers.define :include_cards do |*cards|
