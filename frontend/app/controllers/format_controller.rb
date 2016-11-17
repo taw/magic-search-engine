166566b064529abe1c30a64561d3fb80d06bae59
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-11-17 05:27:38 +0000

    Formats listings work with Blocks / Standard / Modern

--- frontend/app/controllers/format_controller.rb
+++ frontend/app/controllers/format_controller.rb
@@ -5,4 +5,28 @@ class FormatController < ApplicationController
     end
     @title = "Formats"
   end
+
+  def show
+    @format = Format[params[:id].gsub("-", " ")].new
+    if @format.format_pretty_name == "Unknown"
+      render_404
+      return
+    end
+    @title = @format.format_pretty_name
+    @banned = search_best_printings(%Q[banned:"#{@format.format_pretty_name}"])
+    @restricted = search_best_printings(%Q[restricted:"#{@format.format_pretty_name}"])
+    if @format.included_sets
+      @included_sets = @format.included_sets.map{|set_code| $CardDatabase.sets[set_code] }.reverse
+    end
+  end
+
+  private
+
+  def search_best_printings(query)
+    results = $CardDatabase.search(query).printings
+    results.group_by(&:name).sort.map do |name, printings|
+      best_printing = printings.find{|cp| ApplicationHelper.card_picture_path(cp) } || printings[0]
+      best_printing
+    end
+  end
 end
--- frontend/test/controllers/format_controller_test.rb
+++ frontend/test/controllers/format_controller_test.rb
@@ -4,8 +4,37 @@ class FormatControllerTest < ActionController::TestCase
   test "index" do
     get "index"
     assert_response 200
-    assert_select %Q[li:contains("Commander")]
-    assert_select %Q[li:contains("Modern")]
-    assert_select %Q[li:contains("Shards of Alara Block")]
+    assert_select %Q[a:contains("Commander")]
+    assert_select %Q[a:contains("Modern")]
+    assert_select %Q[a:contains("Shards of Alara Block")]
+  end
+
+  test "show - fake format" do
+    get "show", id: "return-to-homelands-block"
+    assert_response 404
+  end
+
+  test "show - Scars of Mirrodin Block" do
+    get "show", id: "scars-of-mirrodin-block"
+    assert_response 200
+    assert_select %Q[title:contains("Scars of Mirrodin Block")]
+    assert_select %Q[h3:contains("Scars of Mirrodin Block")]
+    assert_select %Q[a:contains("Scars of Mirrodin")]
+    assert_select %Q[a:contains("Mirrodin Besieged")]
+    assert_select %Q[a:contains("New Phyrexia")]
+    assert_select %Q[p:contains("There are no banned cards.")]
+    assert_select %Q[p:contains("There are no restricted cards.")]
+  end
+
+  test "show - Innistrad Block" do
+    get "show", id: "innistrad-block"
+    assert_response 200
+    assert_select %Q[title:contains("Innistrad Block")]
+    assert_select %Q[h3:contains("Innistrad Block")]
+    assert_select %Q[a:contains("Innistrad")]
+    assert_select %Q[a:contains("Dark Ascension")]
+    assert_select %Q[a:contains("Avacyr Restored")]
+    assert_select %Q[a:contains("Lingering Souls")]
+    assert_select %Q[p:contains("There are no restricted cards.")]
   end
 end
