941a06f5c64138f8b716d23c28664045386f086c
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-11-17 05:27:38 +0000

    Formats listings work with Blocks / Standard / Modern

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "set" => "set#index"
   get "artist/:id" => "artist#show"
   get "artist" => "artist#index"
+  get "format/:id" => "format#show"
   get "format" => "format#index"
   get "help/syntax" => "help#syntax"
   get "help/rules" => "help#rules"
