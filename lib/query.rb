9fd5b04cca4fef36f818dff6590f942879a50524
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-01-21 17:55:41 +0000

    Moved handling of "time:1997" from Query to QueryTokenizer

--- lib/query.rb
+++ lib/query.rb
@@ -11,24 +11,6 @@ end
 class Query
   def initialize(query_string)
     @cond, @metadata = QueryParser.new.parse(query_string)
-
-    case @metadata[:time]
-    when /\A\d{4}\z/
-      # It would probably be easier if we had end-of-period semantics, but we'd need to hack Date.parse for it
-      # It parses "March 2010" as "2010.3.1"
-      @metadata[:time] = Date.parse("#{@metadata[:time]}.1.1")
-    when /\A\d{4}\.\d{1,2}\z/
-      @metadata[:time] = Date.parse("#{@metadata[:time]}.1")
-    when /\d{4}/
-      # throw at Date.parse but only if not set name / symbol
-      begin
-        @metadata[:time] = Date.parse(@metadata[:time])
-      rescue
-      end
-    else
-      # OK
-    end
-
     if @metadata[:time]
       @cond = ConditionAnd.new(@cond, ConditionPrint.new("<=", @metadata[:time]))
     end
