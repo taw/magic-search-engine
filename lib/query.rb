30f0436d6845b7c9eb322ce4a9fba2b140d693ac
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-02-25 00:36:42 +0000

    Random shuffle

--- lib/query.rb
+++ lib/query.rb
@@ -1,5 +1,6 @@
 require_relative "query_parser"
 require_relative "search_results"
+require "digest"
 
 class Date
   # Any kind of key for sorting
@@ -10,6 +11,7 @@ end
 
 class Query
   def initialize(query_string)
+    @query_string = query_string
     @cond, @metadata = QueryParser.new.parse(query_string)
     # puts "Parse #{query_string} -> #{@cond}"
   end
@@ -45,6 +47,8 @@ class Query
         [c.power ? 0 : 1, -c.power.to_i]
       when "tou"
         [c.toughness ? 0 : 1, -c.toughness.to_i]
+      when "rand"
+        [Digest::MD5.hexdigest(@query_string + c.name)]
       else # "name" or unknown key
         []
       end + [
