5f6ee71cf3cfeba6a7634344f55c4ba873234961
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-05-03 17:35:29 +0100

    Remove some pry references

--- lib/card.rb
+++ lib/card.rb
@@ -153,7 +153,6 @@ class Card
       when "*", "*²", "1+*", "2+*", "7-*"
         val
       else
-        require 'pry'; binding.pry
         raise "Unrecognized value #{val}"
       end
     elsif val.to_i == val.to_f
