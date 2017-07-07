ceff6c8a753beb482e04990242c50319d8c9d10b
Fenhl <fenhl@fenhl.net>
2017-07-07 18:41:56 +0000

    Fix Custom Standard rotation date

--- lib/format/format_custom_standard.rb
+++ lib/format/format_custom_standard.rb
@@ -10,7 +10,7 @@ class FormatCustomStandard < FormatStandard
   def rotation_schedule
     {
       "2016-12-08" => ["ayr", "dms", "ank"],
-      "2017-12-21" => ["ayr", "dms", "ank", "ldo", "tsl"]
+      "2017-04-21" => ["ayr", "dms", "ank", "ldo", "tsl"]
     }
   end
 end
