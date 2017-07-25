fd7eecd950e4d9d8c046960cbce172f85e0ccbc3
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-07-25 03:34:37 +0100

    Use a bunch of separate json files for mtgjson data

--- indexer/lib/card_sets_data.rb
+++ indexer/lib/card_sets_data.rb
@@ -1,7 +1,24 @@
 class CardSetsData
   def initialize
-    json_path = Indexer::ROOT + "AllSets-x.json"
-    @data = JSON.parse(json_path.read)
+    @data = set_paths.map{|path| load_path(path)}.sort_by{|set_code, set|
+      [order.index(set_code) || order.size, set["releaseDate"], set_code]
+    }.to_h
+  end
+
+  def order
+    @order ||= (sets_path + "order.txt").readlines.map(&:chomp)
+  end
+
+  def sets_path
+    Indexer::ROOT + "sets"
+  end
+
+  def set_paths
+    @set_paths ||= sets_path.glob("*.json")
+  end
+
+  def load_path(path)
+    [path.basename(".json").to_s, JSON.parse(path.read)]
   end
 
   def each_set(&block)
