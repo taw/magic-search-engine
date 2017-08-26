dae0ed27ec7a1bc74a218685eb355e7e7eaa8b33
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-08-26 05:14:29 +0100

    Artist regexp search

--- search-engine/spec/regexp_spec.rb
+++ search-engine/spec/regexp_spec.rb
@@ -1,12 +1,22 @@
 describe "Regexp" do
   include_context "db"
 
-  it "handles parse errors" do
-    Query.new('o:/\d+/').warnings.should eq([])
-    Query.new('o:/[a-z/').warnings.should eq(["bad regular expression in o:/[a-z/ - premature end of char-class: /[a-z/i"])
-    Query.new('o:/[a-z]/').warnings.should eq([])
-    Query.new('FT:/[a-z/').warnings.should eq(["bad regular expression in FT:/[a-z/ - premature end of char-class: /[a-z/i"])
-    Query.new('FT:/[a-z]/').warnings.should eq([])
+  context "handles parse errors" do
+    it "o:" do
+      Query.new('o:/\d+/').warnings.should eq([])
+      Query.new('o:/[a-z/').warnings.should eq(["bad regular expression in o:/[a-z/ - premature end of char-class: /[a-z/i"])
+      Query.new('o:/[a-z]/').warnings.should eq([])
+    end
+
+    it "ft:" do
+      Query.new('FT:/[a-z/').warnings.should eq(["bad regular expression in FT:/[a-z/ - premature end of char-class: /[a-z/i"])
+      Query.new('FT:/[a-z]/').warnings.should eq([])
+    end
+
+    it "a:" do
+      Query.new('a:/[a-z/').warnings.should eq(["bad regular expression in a:/[a-z/ - premature end of char-class: /[a-z/i"])
+      Query.new('a:/[a-z]/').warnings.should eq([])
+    end
   end
 
   it "handles timeouts" do
@@ -40,5 +50,15 @@ describe "Regexp" do
     assert_search_equal 'ft:/land/', 'FT:/(?i)land/'
     assert_search_differ 'ft:/land/', 'FT:/(?-i)land/'
     assert_search_differ 'ft:/land/', 'FT:/\bland\b/'
+  end
+
+  it "regexp artist text" do
+    db.search("a:/.{40}/").printings.map(&:artist_name).should eq([
+      "Jim “Stop the Da Vinci Beatdown” Pavelec",
+      "Edward P. Beard, Jr. & Anthony S. Waters",
+      "Pete “Yes the Urza's Legacy One” Venters",
+      "Alan “Don't Feel Like You Have to Pick Me” Pollack",
+      "Edward P. Beard, Jr. & Anthony S. Waters",
+    ])
   end
 end
