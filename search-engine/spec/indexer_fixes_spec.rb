a3f1dafe90c9ef345aa20a2dcbdd36e85155c790
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-09-14 22:20:13 +0100

    Fixed Coldsnap Theme Decks

--- search-engine/spec/indexer_fixes_spec.rb
+++ search-engine/spec/indexer_fixes_spec.rb
@@ -14,4 +14,66 @@ describe "Indexer Fixes Test" do
     end
     (search("e:itp -r:rare -r:uncommon -r:common -r:basic")).should be_empty
   end
+
+  it "Coldsnap Theme Decks" do
+    (search("e:cstd -r:uncommon -r:common -r:basic")).should be_empty
+
+    assert_search_results "e:cstd r:common",
+      "Aurochs",
+      "Barbed Sextant",
+      "Brainstorm",
+      "Casting of Bones",
+      "Dark Banishing",
+      "Dark Ritual",
+      "Deadly Insect",
+      "Disenchant",
+      "Essence Flare",
+      "Gangrenous Zombies",
+      "Gorilla Shaman",
+      "Incinerate",
+      "Insidious Bookworms",
+      "Kjeldoran Dead",
+      "Kjeldoran Pride",
+      "Lat-Nam's Legacy",
+      "Legions of Lim-Dul",
+      "Mistfolk",
+      "Orcish Lumberjack",
+      "Phantasmal Fiend",
+      "Portent",
+      "Reinforcements",
+      "Snow Devil",
+      "Soul Burn",
+      "Tinder Wall",
+      "Woolly Mammoths",
+      "Zuran Spellcaster"
+
+    assert_search_results "e:cstd r:uncommon",
+      "Arcum's Weathervane",
+      "Ashen Ghoul",
+      "Balduvian Dead",
+      "Binding Grasp",
+      "Bounty of the Hunt",
+      "Browse",
+      "Death Spark",
+      "Drift of the Dead",
+      "Giant Trap Door Spider",
+      "Iceberg",
+      "Kjeldoran Elite Guard",
+      "Kjeldoran Home Guard",
+      "Orcish Healer",
+      "Scars of the Veteran",
+      "Skull Catapult",
+      "Storm Elemental",
+      "Swords to Plowshares",
+      "Viscerid Drone",
+      "Whalebone Glider",
+      "Wings of Aesthir"
+
+    assert_search_results "e:cstd r:basic",
+      "Forest",
+      "Island",
+      "Mountain",
+      "Plains",
+      "Swamp"
+  end
 end
