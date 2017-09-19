0c705fefec2b446c57faaca8fda4cffffe8a8113
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-09-19 13:54:16 +0100

    r: aliases

--- search-engine/lib/condition/condition_rarity.rb
+++ search-engine/lib/condition/condition_rarity.rb
@@ -2,6 +2,13 @@ class ConditionRarity < ConditionSimple
   def initialize(op, rarity)
     @op = op
     @rarity = rarity.downcase
+    @rarity = "basic" if @rarity == "b"
+    @rarity = "basic" if @rarity == "l"
+    @rarity = "common" if @rarity == "c"
+    @rarity = "uncommon" if @rarity == "u"
+    @rarity = "rare" if @rarity == "r"
+    @rarity = "mythic" if @rarity == "m"
+    @rarity = "special" if @rarity == "s"
     @rarity_code = %W[basic common uncommon rare mythic special].index(@rarity) or raise "Unknown rarity #{@rarity}"
   end
 
