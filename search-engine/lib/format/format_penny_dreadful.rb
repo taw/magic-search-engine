6a1a82418d2e3605ef54ae67f029aeaf65978e08
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-09-30 08:05:46 +0100

    Penny Dreadful Brisela fix

--- search-engine/lib/format/format_penny_dreadful.rb
+++ search-engine/lib/format/format_penny_dreadful.rb
@@ -4,8 +4,8 @@ class FormatPennyDreadful < FormatVintage
       next if @time and printing.release_date > @time
       return true if PrimaryCards.include?(printing.name)
       if printing.others
-        printing.others.each do |other|
-          return true if PrimaryCards.include?(other.name)
+        return true if printing.others.all? do |other|
+          PrimaryCards.include?(other.name)
         end
       end
     end
@@ -24,17 +24,5 @@ class FormatPennyDreadful < FormatVintage
     }.to_set
   end
 
-  # Awkwardly preprocessing this list requires full database
-  def self.all_cards(db)
-    result = PrimaryCards.dup
-    PrimaryCards.each do |card_name|
-      printing = db.cards[card_name.downcase].printings[0]
-      next unless printing.others
-      printing.others.each do |other|
-        result << other.name
-      end
-    end
-    result
-  end
   PrimaryCards = load_cards
 end
