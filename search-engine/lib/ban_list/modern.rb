a0b6510ae1b3cbbcb1a5d9c8aedcdd26e4f6b7d9
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-10-23 17:13:49 +0100

    2012+ banlist links for official formats

--- search-engine/lib/ban_list/modern.rb
+++ search-engine/lib/ban_list/modern.rb
@@ -37,33 +37,33 @@ BanList.for_format("modern") do
 
   change(
     "2012-01-01",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/feature/december-20-2011-dci-banned-restricted-list-announcement-2011-12-20",
     "Punishing Fire" => "banned",
     "Wild Nacatl" => "banned",
   )
 
   change(
     "2012-10-01",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/feature/september-20-2012-dci-banned-restricted-list-announcement-2012-09-20",
     "Valakut, the Molten Pinnacle" => "legal",
   )
 
   change(
     "2013-02-01",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/january-28-2013-dci-banned-restricted-list-announcement-2013-01-28",
     "Bloodbraid Elf" => "banned",
     "Seething Song" => "banned",
   )
 
   change(
     "2013-05-03",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/feature/banned-and-restricted-2013-04-22-0",
     "Second Sunrise" => "banned",
   )
 
   change(
     "2014-02-07",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/top-decks/february-3-2014-dci-banned-restricted-list-announcement-2014-02-03",
     "Deathrite Shaman" => "banned",
     "Bitterblossom" => "legal",
     "Wild Nacatl" => "legal",
@@ -71,7 +71,7 @@ BanList.for_format("modern") do
 
   change(
     "2015-01-23",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/feature/banned-and-restricted-announcement-2015-01-19",
     "Birthing Pod" => "banned",
     "Dig Through Time" => "banned",
     "Treasure Cruise" => "banned",
@@ -80,14 +80,14 @@ BanList.for_format("modern") do
 
   change(
     "2016-01-22",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/news/january-18-2016-banned-and-restricted-announcement-2016-01-18",
     "Splinter Twin" => "banned",
     "Summer Bloom" => "banned",
   )
 
   change(
     "2016-04-08",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/news/banned-and-restricted-announcement-2016-04-04",
     "Eye of Ugin" => "banned",
     "Ancestral Vision" => "legal",
     "Sword of the Meek" => "legal",
@@ -95,7 +95,7 @@ BanList.for_format("modern") do
 
   change(
     "2017-01-20",
-    nil,
+    "https://magic.wizards.com/en/articles/archive/news/january-9-2017-banned-and-restricted-announcement-2017-01-09",
     "Gitaxian Probe" => "banned",
     "Golgari Grave-Troll" => "banned",
   )
