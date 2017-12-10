31b5e7ecb2dffc01060d8224cfcab95fc47b9d10
Fenhl <fenhl@fenhl.net>
2017-12-10 01:59:53 +0000

    Update Custom Commander definition

--- search-engine/lib/format/format_custom_commander.rb
+++ search-engine/lib/format/format_custom_commander.rb
@@ -1,5 +1,29 @@
-class FormatCustomCommander < FormatCustomEternal
+class FormatCustomCommander < Format
+  def initialize(time=nil)
+    super(time)
+    @real_ban_list = BanList["commander"]
+    @custom_ban_list = BanList["custom eternal"]
+  end
+
   def format_pretty_name
     "Custom Commander"
+  end
+
+  def include_custom_sets?
+    true
+  end
+
+  def build_included_sets
+    FormatCommander.build_included_sets | FormatCustomEternal.build_included_sets
+  end
+
+  def legality(card)
+    status = super(card)
+    return status if status != "legal"
+    if card.custom?
+      @custom_ban_list.legality(card.name, @time)
+    else
+      @real_ban_list.legality(card.name, @time)
+    end
   end
 end
