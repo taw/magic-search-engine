5f0b26bb438e61051a1bdcf12b55387fd4e9822e
Fenhl <fenhl@fenhl.net>
2017-12-11 05:40:30 +0000

    Fix FormatCommander constructor call

--- search-engine/lib/format/format_custom_commander.rb
+++ search-engine/lib/format/format_custom_commander.rb
@@ -14,7 +14,7 @@ class FormatCustomCommander < Format
   end
 
   def build_excluded_sets
-    FormatCommander[@time].build_excluded_sets
+    Format["commander"].new(@time).build_excluded_sets
   end
 
   def legality(card)
