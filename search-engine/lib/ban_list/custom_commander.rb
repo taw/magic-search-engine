7cf73db1a5d69b3afad1ebcd580c6d877f533821
Fenhl <fenhl@fenhl.net>
2017-12-12 00:05:18 +0000

    Update Custom Commander banlist

---
+++ search-engine/lib/ban_list/custom_commander.rb
@@ -0,0 +1,12 @@
+BanList.for_format("custom commander") do
+  format_start(
+    nil,
+    "Dreamheart Mox" => "banned",
+  )
+
+  change(
+    "2017-12-11",
+    nil,
+    "Hatseket, Great Archivist" => "banned",
+  )
+end
