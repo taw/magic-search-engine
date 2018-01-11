f70b1d71ad6a08d772fa03705c9a0095e7bb0200
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-01-11 16:18:47 +0000

    Rivals of Ixalan

--- search-engine/spec/format_modern_spec.rb
+++ search-engine/spec/format_modern_spec.rb
@@ -13,7 +13,7 @@ describe "Formats - Modern" do
   end
 
   it "modern" do
-    assert_block_composition "modern", "xln", ["8e", "mi", "ds", "5dn", "chk", "bok", "sok", "9e", "rav", "gp", "di", "cs", "ts", "tsts", "pc", "fut", "10e", "lw", "mt", "shm", "eve", "ala", "cfx", "arb", "m10", "zen", "wwk", "roe", "m11", "som", "mbs", "nph", "m12", "isd", "dka", "avr", "m13", "rtr", "gtc", "dgm", "m14", "ths", "bng", "jou", "m15", "ktk", "frf", "dtk", "ori", "bfz", "ogw", "soi", "emn", "kld", "aer", "akh", "w17", "hou", "xln"],
+    assert_block_composition "modern", "rix", ["8e", "mi", "ds", "5dn", "chk", "bok", "sok", "9e", "rav", "gp", "di", "cs", "ts", "tsts", "pc", "fut", "10e", "lw", "mt", "shm", "eve", "ala", "cfx", "arb", "m10", "zen", "wwk", "roe", "m11", "som", "mbs", "nph", "m12", "isd", "dka", "avr", "m13", "rtr", "gtc", "dgm", "m14", "ths", "bng", "jou", "m15", "ktk", "frf", "dtk", "ori", "bfz", "ogw", "soi", "emn", "kld", "aer", "akh", "w17", "hou", "xln", "rix"],
       "Ancient Den" => "banned",
       "Birthing Pod" => "banned",
       "Blazing Shoal" => "banned",
