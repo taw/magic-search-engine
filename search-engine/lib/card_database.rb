160c7201153ebe60aec23a0860749c1046ed551f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-01-15 13:57:26 +0000

    Some trivial changes based on feedback from fasterer gem; doubtful the speedup will be noticable

--- search-engine/lib/card_database.rb
+++ search-engine/lib/card_database.rb
@@ -237,7 +237,7 @@ class CardDatabase
   def spelling_suggestions
     @spelling_suggestions ||= begin
       ss = SpellingSuggestions.new
-      @cards.keys.each do |title|
+      @cards.each_key do |title|
         ss << title
       end
       ss
