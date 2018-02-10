b813cacba684baaaf686628dbfed77afdc502ad3
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-02-10 23:34:22 +0000

    any: support for every is: - some of them questionable

--- search-engine/spec/any_spec.rb
+++ search-engine/spec/any_spec.rb
@@ -37,32 +37,26 @@ describe "Any queries" do
 
   it "includes Russian name" do
     assert_search_equal %Q[any:"Кровавая луна"], %Q[ru:"Кровавая луна"]
-
   end
 
   it "includes Spanish name" do
     assert_search_equal %Q[any:"Puente engañoso"], %Q[sp:"Puente engañoso"]
-
   end
 
   it "includes Portuguese name" do
     assert_search_equal %Q[any:"Ponte Traiçoeira"], %Q[pt:"Ponte Traiçoeira"]
-
   end
 
   it "includes Korean name" do
     assert_search_equal %Q[any:"축복받은 신령들"], %Q[kr:"축복받은 신령들"]
-
   end
 
   it "includes Chinese Traditional name" do
     assert_search_equal %Q[any:"刻拉諾斯的電擊"], %Q[ct:"刻拉諾斯的電擊"]
-
   end
 
   it "includes Chinese Simplified name" do
     assert_search_equal %Q[any:"刻拉诺斯的电击"], %Q[cs:"刻拉诺斯的电击"]
-
   end
 
   context "artist" do
@@ -147,6 +141,108 @@ describe "Any queries" do
       assert_search_equal %Q[any:"-1/3"], "pow=-1 tou=3"
       # This is very problematic as -1/-1 is very common in Oracle text
       assert_search_equal %Q[any:"-1/-1"], %Q[(pow=-1 tou=-1) or o:"-1/-1"]
+    end
+  end
+
+  context "is:" do
+    it "augment" do
+      assert_search_equal "any:augment", "is:augment or o:augment or ft:augment or (augment) or foreign:augment"
+    end
+
+    it "battleland" do
+      assert_search_equal "any:battleland", "is:battleland"
+    end
+
+    it "bounceland" do
+      assert_search_equal "any:bounceland", "is:bounceland"
+    end
+
+    it "checkland" do
+      assert_search_equal "any:checkland", "is:checkland"
+    end
+
+    it "commander" do
+      assert_search_equal "any:commander", "is:commander or ft:commander or o:commander or (commander) or foreign:commander"
+    end
+
+    it "digital" do
+      assert_search_equal "any:digital", "is:digital or foreign:digital"
+    end
+
+    it "dual" do
+      assert_search_equal "any:dual", "is:dual or (dual) or ft:dual"
+    end
+
+    it "fastland" do
+      assert_search_equal "any:fastland", "is:fastland"
+    end
+
+    it "fetchland" do
+      assert_search_equal "any:fetchland", "is:fetchland"
+    end
+
+    it "filterland" do
+      assert_search_equal "any:filterland", "is:filterland"
+    end
+
+    it "funny" do
+      assert_search_equal "any:funny", "is:funny or ft:funny"
+    end
+
+    it "gainland" do
+      assert_search_equal "any:gainland", "is:gainland"
+    end
+
+    it "manland" do
+      assert_search_equal "any:manland", "is:manland"
+    end
+
+    it "multipart" do
+      assert_search_equal "any:multipart", "is:multipart"
+    end
+
+    it "permanent" do
+      assert_search_equal "any:permanent", "is:permanent or o:permanent or ft:permanent"
+    end
+
+    it "primary" do
+      assert_search_equal "any:primary", "is:primary"
+    end
+
+    it "promo" do
+      assert_search_equal "any:promo", "is:promo or foreign:promo"
+    end
+
+    it "reprint" do
+      assert_search_equal "any:reprint", "is:reprint"
+    end
+
+    it "reserved" do
+      assert_search_equal "any:reserved", "is:reserved or ft:reserved"
+    end
+
+    it "scryland" do
+      assert_search_equal "any:scryland", "is:scryland"
+    end
+
+    it "shockland" do
+      assert_search_equal "any:shockland", "is:shockland"
+    end
+
+    it "spell" do
+      assert_search_equal "any:spell", "is:spell or o:spell"
+    end
+
+    it "timeshifted" do
+      assert_search_equal "any:timeshifted", "is:timeshifted"
+    end
+
+    it "unique" do
+      assert_search_equal "any:unique", "is:unique or ft:unique"
+    end
+
+    it "vanilla" do
+      assert_search_equal "any:vanilla", "is:vanilla"
     end
   end
 end
