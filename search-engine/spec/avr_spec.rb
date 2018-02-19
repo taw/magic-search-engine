775e5f00dbfc48cd1e36426088e8cfba47702a63
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-02-19 22:10:18 +0000

    Foreign language regexp search

--- search-engine/spec/avr_spec.rb
+++ search-engine/spec/avr_spec.rb
@@ -13,6 +13,7 @@ describe "Avacyn Restored" do
     assert_search_results "cs:拱翼巨龙", "Archwing Dragon"
     assert_search_results "tw:拱翼巨龙"
     assert_search_results "ct:拱翼巨龙"
+    # @
   end
 
   it "chinese_traditional" do
@@ -20,12 +21,14 @@ describe "Avacyn Restored" do
     assert_search_results "tw:拱翼巨龍", "Archwing Dragon"
     assert_search_results "cs:拱翼巨龍"
     assert_search_results "cn:拱翼巨龍"
+    # @
   end
 
   it "fr" do
     assert_search_results "fr:Fragments", "Bone Splinters"
     assert_search_results %Q[fr:"Lumière d'albâtre"], "Bruna, Light of Alabaster"
     assert_search_results %Q[fr:"lumiere d'albatre"], "Bruna, Light of Alabaster"
+    #@
   end
 
   it "de" do
@@ -40,22 +43,53 @@ describe "Avacyn Restored" do
       "Emancipation Angel",
       "Entreat the Angels",
       "Restoration Angel"
+    assert_search_results %q[de:/\bdes\b/],
+      "Alchemist's Refuge",
+      "Angel of Glory's Rise",
+      "Angel's Mercy",
+      "Angel's Tomb",
+      "Builder's Blessing",
+      "Commander's Authority",
+      "Conjurer's Closet",
+      "Demonlord of Ashmouth",
+      "Druid's Familiar",
+      "Herald of War",
+      "Midvast Protector",
+      "Predator's Gambit",
+      "Rush of Blood",
+      "Terrifying Presence",
+      "Tormentor's Trident"
   end
 
   it "it" do
     assert_search_results "it:clemenza", "Angel's Mercy"
+    assert_search_results %q[it:/\bdell\b/],
+      "Alchemist's Apprentice",
+      "Alchemist's Refuge",
+      "Angel of Glory's Rise",
+      "Angel's Mercy",
+      "Angel's Tomb",
+      "Conjurer's Closet",
+      "Essence Harvest",
+      "Seraph of Dawn",
+      "Timberland Guide",
+      "Treacherous Pit-Dweller",
+      "Vanguard's Shield"
   end
 
   it "jp" do
     assert_search_results "jp:ブルーナ", "Bruna, Light of Alabaster"
+    assert_search_results %q[jp:/ブルーナ/], "Bruna, Light of Alabaster"
   end
 
   it "kr" do
     assert_search_results "kr:아바신", "Avacyn, Angel of Hope", "Scroll of Avacyn"
+    assert_search_results %q[kr:/아바신/], "Avacyn, Angel of Hope", "Scroll of Avacyn"
   end
 
   it "pt" do
     assert_search_results "pt:estacas", "Bone Splinters"
+    assert_search_results %q[pt:/estacas/], "Bone Splinters"
   end
 
   it "ru" do
@@ -73,9 +107,11 @@ describe "Avacyn Restored" do
       "Restoration Angel"
     assert_search_equal "ru:ангел", "ru:Ангел"
     assert_search_equal "ru:АНГЕЛ", "ru:Ангел"
+    assert_search_equal %q[ru:/АНГЕЛ/], "ru:Ангел"
   end
 
   it "sp" do
     assert_search_results "sp:Astillas", "Bone Splinters"
+    assert_search_results %q[sp:/astillas/], "Bone Splinters"
   end
 end
