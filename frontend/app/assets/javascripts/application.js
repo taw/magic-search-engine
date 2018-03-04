f1453c0422defdca1945ee7e28fbc8acf3879407
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-03-04 01:51:15 +0000

    Trying to reenable turbolinks

--- frontend/app/assets/javascripts/application.js
+++ frontend/app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .
 
 $(document).on("mouseover", ".previewable_card_name", function() {
