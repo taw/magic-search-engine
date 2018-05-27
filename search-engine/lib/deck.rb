8a98ff855a5fa22b3f486e18b406ab1eca26ed31
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-05-27 12:51:40 +0100

    Show list of preconstructed decks

--- search-engine/lib/deck.rb
+++ search-engine/lib/deck.rb
@@ -1,9 +1,18 @@
 class Deck
+  attr_reader :set, :name, :type, :card, :sideboard
   def initialize(set, name, type, cards, sideboard)
     @set = set
     @name = name
     @type = type
     @cards = cards
     @sideboard = sideboard
+  end
+
+  def number_of_cards
+    @cards.map(&:first).inject(0, &:+)
+  end
+
+  def number_of_sideboard_cards
+    @sideboard.map(&:first).inject(0, &:+)
   end
 end
