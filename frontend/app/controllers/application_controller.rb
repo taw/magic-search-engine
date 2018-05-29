872fc2200da9e8c2019e46286288853a14c27b2c
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-05-29 01:30:42 +0100

    Oops, render_403 is not standard rails

--- frontend/app/controllers/application_controller.rb
+++ frontend/app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     render file: "#{Rails.root}/public/404.html", layout: false, status: 404
   end
 
+  def render_403
+    render file: "#{Rails.root}/public/403.html", layout: false, status: 403
+  end
+
   private
 
   def paginate_by_set(printings, page)
