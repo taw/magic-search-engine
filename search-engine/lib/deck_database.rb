61d6894a69b2f68e126c33e35c041bbd2c674d76
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-05-29 23:30:08 +0100

    Precons now have foiling information

--- search-engine/lib/deck_database.rb
+++ search-engine/lib/deck_database.rb
@@ -3,11 +3,11 @@ class DeckDatabase
     @db = db
   end
 
-  def resolve_card(count, set_code, card_number)
+  def resolve_card(count, set_code, card_number, foil=false)
     set = @db.sets[set_code] or raise "Set not found #{set_code}"
     printing = set.printings.find{|cp| cp.number == card_number}
     raise "Card not found #{sc}/#{number}" unless printing
-    [count, PhysicalCard.for(printing)]
+    [count, PhysicalCard.for(printing, !!foil)]
   end
 
   def load!(path=Pathname("#{__dir__}/../../index/deck_index.json"))
