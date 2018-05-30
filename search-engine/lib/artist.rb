5411ee7aa0a2a33b0e6ef4bd0292fea8d4b50893
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-05-30 02:05:22 +0100

    More speculative artist CJK support

--- search-engine/lib/artist.rb
+++ search-engine/lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
 
   def initialize(name)
     @name = name
-    @slug = name.downcase.gsub(/[^a-z\p{Han}\p{Katakana}\p{Hiragana}\p{Hangul}]+/, "_")
+    @slug = name.downcase.gsub(/[^a-z0-9\p{Han}\p{Katakana}\p{Hiragana}\p{Hangul}]+/, "_")
     @printings = Set[]
   end
 
