abd73938f480a027fd5543458771231cfe3e5297
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-06-04 20:05:18 +0100

    Support opening multiple pack types (3 in UI, more in controller)

--- frontend/test/controllers/sealed_controller_test.rb
+++ frontend/test/controllers/sealed_controller_test.rb
@@ -6,4 +6,11 @@ class SealedControllerTest < ActionController::TestCase
     assert_response 200
     assert_equal "Sealed - mtg.wtf", html_document.title
   end
+
+  test "open some packs" do
+    get "index", params: {count: ["1", "2"], set: ["nph", "an"]}
+    assert_response 200
+    assert_equal "Sealed - mtg.wtf", html_document.title
+    assert_select ".card_picture_container", count: 15 + 2*8
+  end
 end
