540507bb04a12faab3d63b1aef6d60ddf3df0d07
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-06-04 23:45:00 +0100

    Link by card set code / number not name to avoid issues with any funny characters

--- frontend/app/assets/javascripts/application.js
+++ frontend/app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 $(document).on("mouseover", ".previewable_card_name", function() {
   var preview_link = $(this).data("preview-link");
   $(this).closest(".decklist").find(".card_picture_cell").hide();
-  $(this).closest(".decklist").find(".card_picture_cell[data-preview='"+preview_link.replace("'", "\\'")+"']").show();
+  $(this).closest(".decklist").find(".card_picture_cell[data-preview='"+preview_link+"']").show();
 })
 
 $(document).on("scroll", function() {
