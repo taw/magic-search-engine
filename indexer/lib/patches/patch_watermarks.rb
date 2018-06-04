5dec557e226a2c696082319407edfac8ff5446a2
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-06-04 08:50:35 +0100

    Preserve watermark capitalization

--- indexer/lib/patches/patch_watermarks.rb
+++ indexer/lib/patches/patch_watermarks.rb
@@ -6,8 +6,7 @@ class PatchWatermarks < Patch
       if %W[White Blue Black Red Green Colorless].include?(watermark)
         card.delete("watermark")
       else
-        # FIXME: We need to be a lot smarter than this, Unstable watermarks have weird capitalization we need to preserve
-        card["watermark"] = watermark.downcase
+        card["watermark"] = watermark
       end
     end
   end
