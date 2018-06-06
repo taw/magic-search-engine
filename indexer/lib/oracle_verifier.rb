1ea55e609adb40bfb7841720229ba96b1f265262
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-06-06 14:16:15 +0100

    Simplify OracleVerifier API (it still does the wrong thing, should just use priority by set)

--- indexer/lib/oracle_verifier.rb
+++ indexer/lib/oracle_verifier.rb
@@ -2,103 +2,82 @@ class OracleVerifier
   def initialize(printings, field_name)
     @printings = printings
     @field_name = field_name
-    # Refactor this away
-    @name = @printings[0]["name"]
-    @versions = []
-    @canonical = nil
-  end
-
-  def add_all!
-    @printings.each do |printing|
-      @versions << [printing["set_code"], printing.slice(@field_name)]
-    end
-  end
-
-  def verify_key!(key)
-    variants = {}
-    @versions.each do |set_code, version|
-      variant = version[key]
-      variants[variant] ||= []
-      variants[variant] << set_code
-    end
-    canonical_variant = nil
-    canonical_variant_source = nil
-
-    if variants.size == 1
-      # This key is fine
-      canonical_variant = variants.keys[0]
-    else
-      # This key is broken
-      # We need to fix it
-      if key == "rulings"
-        # That's low value fail, would be nice if they fixed it, but whatever
-        canonical_variant = variants.keys.max_by{|v| v.to_s.size}
-      elsif key == "subtypes"
-        if @name == "Aesthir Glider"
-          canonical_variant_source = "dom"
-        end
-      else
-        # Transition
-        rx = /his or her|him or her|he or she|mana pool|target creature or player/i
-        if variants.keys.any?{|x| x =~ rx }
-          good = variants.keys.select{|x| x !~ rx}
-          if good.size == 1
-            canonical_variant = good[0]
-          end
-        elsif variants.keys.any?{|x| x =~ /this spell/i } and variants.keys.any?{|x| x !~ /this spell/i }
-          good = variants.keys.select{|x| x =~ /this spell/i }
-          if good.size == 1
-            canonical_variant = good[0]
-          end
-        elsif ["Land Tax", "Skyshroud Claim", "Vigor", "Fertilid", "Daggerback Basilisk", "Unflinching Courage", "Plated Crusher"].include?(@name)
-          canonical_variant_source = "bbd"
-        else
-          # first line: never updated (also: UGL,UNH,UST,S00)
-          # second line: not updated yet
-          known_outdated = %W[
-            ced cedi bok st2k v17 cp2
-            rep mbp rqs arena itp at mprp wotc thgt dpa jr cp gtw ptc sus jr fnmp pro mgdc mlp
-          ]
-          maybe_good_variants = {}
-          variants.each do |variant, sets|
-            sets -= known_outdated
-            next if sets.empty?
-            maybe_good_variants[variant] = sets
-          end
-          if maybe_good_variants.size == 1
-            canonical_variant = maybe_good_variants.keys[0]
-          else
-            warn [@name, maybe_good_variants]
-            # binding.pry
-          end
-        end
-      end
-    end
-    if canonical_variant_source
-      canonical_variant = @versions.find{|k,v| k == canonical_variant_source}[1][key]
-    end
-
-    if canonical_variant
-      @canonical = canonical_variant
-    end
-  end
-
-  def verify!
-    # All versions are same, no reason to dig deeper
-    if @versions.map(&:last).uniq.size == 1
-      @canonical = @versions[0][1][@field_name]
-    else
-      # Something failed
-      @canonical = nil
-      verify_key!(@field_name)
-    end
   end
 
   def canonical
-    unless @canonical
-      add_all!
-      verify!
+    # All versions are same, no reason to dig deeper
+    if variants.size == 1
+      variants.keys.first
+    else
+      # Something failed
+      reconcile
     end
-    @canonical
+  end
+
+  private
+
+  def card_name
+    @card_name ||= @printings[0]["name"]
+  end
+
+  def variants
+    unless @variants
+      @variants = {}
+      @printings.each do |printing|
+        set_code = printing["set_code"]
+        variant = printing[@field_name]
+        (@variants[variant] ||= []) << set_code
+      end
+    end
+    @variants
+  end
+
+  def reconcile
+    # There are multiple versions, we need to figure out which works
+
+    if @field_name == "rulings"
+      # That's low value fail, would be nice if they fixed it, but whatever
+      # FIXME: actually not really, "his or her" takes false priority over "their"
+      return @variants.keys.max_by{|v| v.to_s.size}
+    elsif @field_name == "subtypes"
+      if card_name == "Aesthir Glider"
+        canonical_variant_source = "dom"
+      end
+    else
+      # Transition
+      rx = /his or her|him or her|he or she|mana pool|target creature or player/i
+      if @variants.keys.any?{|x| x =~ rx }
+        good = @variants.keys.select{|x| x !~ rx}
+        return good[0] if good.size == 1
+      elsif @variants.keys.any?{|x| x =~ /this spell/i } and @variants.keys.any?{|x| x !~ /this spell/i }
+        good = @variants.keys.select{|x| x =~ /this spell/i }
+        return good[0] if good.size == 1
+      elsif ["Land Tax", "Skyshroud Claim", "Vigor", "Fertilid", "Daggerback Basilisk", "Unflinching Courage", "Plated Crusher"].include?(card_name)
+        canonical_variant_source = "bbd"
+      else
+        # first line: never updated (also: UGL,UNH,UST,S00)
+        # second line: not updated yet
+        known_outdated = %W[
+          ced cedi bok st2k v17 cp2
+          rep mbp rqs arena itp at mprp wotc thgt dpa jr cp gtw ptc sus jr fnmp pro mgdc mlp
+        ]
+        maybe_good_variants = {}
+        @variants.each do |variant, sets|
+          sets -= known_outdated
+          next if sets.empty?
+          maybe_good_variants[variant] = sets
+        end
+        if maybe_good_variants.size == 1
+          return maybe_good_variants.keys[0]
+        else
+          warn [card_name, maybe_good_variants]
+          # binding.pry
+        end
+      end
+    end
+
+    if canonical_variant_source
+      @variants.to_a.find{|variant,sets| sets.include?(canonical_variant_source)}.first
+    end
   end
 end
