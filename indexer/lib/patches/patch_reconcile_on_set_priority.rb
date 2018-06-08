8d1daf03088070fdba2fc620559c73e8f32e946f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-06-08 17:53:56 +0100

    Merged SetPriorityBasedReconciliation into PatchReconcileOnSetPriority

--- indexer/lib/patches/patch_reconcile_on_set_priority.rb
+++ indexer/lib/patches/patch_reconcile_on_set_priority.rb
@@ -1,6 +1,3 @@
-# TODO: Merge SetPriorityBasedReconciliation into this
-require_relative "../set_priority_based_reconciliation"
-
 class PatchReconcileOnSetPriority < Patch
   # If something is missing, it will fail validation later on,
   # only pass what you'd like to see reconciled automatically
@@ -19,8 +16,31 @@ class PatchReconcileOnSetPriority < Patch
   def call
     each_card do |name, printings|
       fields_to_reconcile.each do |field_name|
-        SetPriorityBasedReconciliation.new(printings, field_name).reconcile
+        # All versions are same, or all different versions are low priority
+        variants = max_priority_variants(printings, field_name)
+        if variants.size == 1
+          canonical = variants.keys[0]
+          printings.each do |printing|
+            printing[field_name] = canonical
+          end
+        else
+          conflicting_sets = max_priority_variants.values.map{|scs| scs.join(",") }
+          warn "Can't reconcile #{name}, need to prioritize between #{ conflicting_sets.join(" vs ") }"
+        end
       end
     end
+  end
+
+  def max_priority_variants(printings, field_name)
+    by_priority = {}
+    printings.each do |printing|
+      set_code = printing["set_code"]
+      set_priority = printing["set"]["priority"]
+      variant = printing[field_name]
+      by_priority[set_priority] ||= {}
+      by_priority[set_priority][variant] ||= []
+      by_priority[set_priority][variant] << set_code
+    end
+    by_priority[by_priority.keys.max]
   end
 end
