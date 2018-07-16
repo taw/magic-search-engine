3bea39f2e328753c60c56c7d65ce6b91c4ed9450
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-07-16 10:22:58 +0200

    is:holofoil

---
+++ search-engine/lib/condition/condition_is_holofoil.rb
@@ -0,0 +1,13 @@
+class ConditionIsHolofoil < ConditionSimple
+  def match?(card)
+    return false unless card.frame == "m15"
+    return true if card.set_code == "ust" and card.rarity == "basic"
+    return false unless %W[rare mythic special].include?(card.rarity)
+    return false if card.types.include?("contraption")
+    true
+  end
+
+  def to_s
+    "is:holofoil"
+  end
+end
---
+++ search-engine/spec/holofoil_spec.rb
@@ -0,0 +1,8 @@
+describe "is:holofoil" do
+  include_context "db"
+
+  it do
+    # UST is exception to usual rules as full art basics have holofoil, but rare contraptions don't
+    assert_search_equal "++ is:holofoil", "++ (r>=rare or (e:ust r:basic)) frame=m15 -t:contraption"
+  end
+end
