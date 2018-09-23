b66bc04c61d3c7c3e59ed2c552d69fed7272be08
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-09-23 16:35:33 +0100

    ci queries

--- search-engine/lib/condition/condition_color_expr.rb
+++ search-engine/lib/condition/condition_color_expr.rb
@@ -10,6 +10,7 @@ class ConditionColorExpr < ConditionSimple
       a = card.colors.chars.to_set
     elsif @a == "in"
       a = card.color_indicator_set
+      return false unless a
     else
       a = card.color_identity.chars.to_set
     end
