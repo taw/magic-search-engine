69d02f03529f83a2557b595a1cb83dd0f3928ee5
Fenhl <fenhl@fenhl.net>
2018-09-23 14:11:30 +0000

    Advanced color indicator queries

--- search-engine/lib/card.rb
+++ search-engine/lib/card.rb
@@ -14,7 +14,7 @@ class Card
   attr_reader :name, :names, :layout, :colors, :mana_cost, :reserved, :types
   attr_reader :partial_color_identity, :cmc, :text, :text_normalized, :power, :toughness, :loyalty, :extra
   attr_reader :hand, :life, :rulings, :foreign_names, :foreign_names_normalized, :stemmed_name
-  attr_reader :mana_hash, :typeline, :funny, :color_indicator, :related
+  attr_reader :mana_hash, :typeline, :funny, :color_indicator, :color_indicator_set, :related
   attr_reader :reminder_text, :augment, :display_power, :display_toughness, :display_mana_cost
 
   def initialize(data)
@@ -257,8 +257,10 @@ class Card
 
     if colors_inferred_from_mana_cost.sort == actual_colors.sort
       @color_indicator = nil
+      @color_indicator_set = Set[]
     else
       @color_indicator = color_indicator_name(actual_colors)
+      @color_indicator_set = actual_colors.to_set
     end
   end
 
