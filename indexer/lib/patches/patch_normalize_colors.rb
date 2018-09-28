8e23a937bd39067975b991f942f7dec923fe3a12
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-09-28 01:40:00 +0100

    tiny mtgjson v4 compatibility fix

--- indexer/lib/patches/patch_normalize_colors.rb
+++ indexer/lib/patches/patch_normalize_colors.rb
@@ -1,7 +1,12 @@
 class PatchNormalizeColors < Patch
   def call
     each_printing do |card|
-      color_codes = {"White"=>"w", "Blue"=>"u", "Black"=>"b", "Red"=>"r", "Green"=>"g"}
+      color_codes = {
+        # v3
+        "White"=>"w", "Blue"=>"u", "Black"=>"b", "Red"=>"r", "Green"=>"g",
+        # v4
+        "W"=>"w", "U"=>"u", "B"=>"b", "R"=>"r", "G"=>"g",
+      }
       colors = card["colors"] || []
       card["colors"] = colors.map{|c| color_codes.fetch(c)}.sort.join
     end
