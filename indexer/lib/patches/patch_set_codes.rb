49bae83c4378163bd9470613eeb28f17efff3bc1
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-10-09 23:14:15 +0100

    Start transition to new set codes by lowercasing all setcodes equally

--- indexer/lib/patches/patch_set_codes.rb
+++ indexer/lib/patches/patch_set_codes.rb
@@ -3,20 +3,27 @@
 # with some exceptions in both cases
 class PatchSetCodes < Patch
   def call
+    # Forced lower case for all codes
     each_set do |set|
-      set["code"] ||= set["official_code"].downcase
+      set["code"] = set["code"]&.downcase
+      set["gatherer_code"] = set["gatherer_code"]&.downcase
+      set["official_code"] = set["official_code"]&.downcase
+    end
+
+    each_set do |set|
+      set["code"] ||= set["official_code"]
       case set["official_code"]
-      when "pGTW"
+      when "pgtw"
         set["code"] = "gtw"
-      when "pWPN"
+      when "pwpn"
         set["code"] = "wpn"
-      when "CM1", "CMA", "MPS", "MPS_AKH", "CP1", "CP2", "CP3"
-        set["code"] = set["official_code"].downcase
+      when "cm1", "cma", "mps", "mps_akh", "cp1", "cp2", "cp3"
+        set["code"] = set["official_code"]
       end
       # Only include it if different from both official and MCI codes
       set.delete("gatherer_code") if set["gatherer_code"]&.downcase == set["code"]
       # Delete ones conflicting with MCI codes for different sets
-      set.delete("gatherer_code") if %W[AL ST LE MI].include?(set["gatherer_code"])
+      set.delete("gatherer_code") if %W[al st le mi].include?(set["gatherer_code"])
     end
 
     duplicated_codes = @sets
