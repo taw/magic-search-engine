e457569a9caadd2ebb2358a983dc9beb20ce4880
Fenhl <fenhl@fenhl.net>
2018-10-13 13:39:28 +0000

    Fix db type for snowflake

--- frontend/db/migrate/20181013132911_create_users.rb
+++ frontend/db/migrate/20181013132911_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.integer :uid
+      t.integer :uid, :limit => 8
 
       t.timestamps
     end
