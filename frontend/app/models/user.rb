7a56fcb28656a6310b0331f16b5ca57649f4d184
Fenhl <fenhl@fenhl.net>
2018-10-13 21:06:20 +0000

    Load usernames from profiles dir

--- frontend/app/models/user.rb
+++ frontend/app/models/user.rb
@@ -1,3 +1,6 @@
+require "json"
+require "pathname"
+
 class User < ApplicationRecord
   validates_presence_of :uid
   validates_uniqueness_of :uid
@@ -6,7 +9,27 @@ class User < ApplicationRecord
     find_or_create_by!(uid: hash['uid'])
   end
 
+  def profile_data
+    user_path = Pathname.new("/var/www/loreseeker.fenhl.net/profiles").children.flat_map do |guild_dir|
+      user_path = guild_dir + "#{self.uid}.json"
+      if user_path.exist?
+        [user_path]
+      else
+        []
+      end
+    end.first
+    JSON.load(user_path)
+  end
+
+  def username
+    profile_data["username"]
+  end
+
+  def discrim
+    profile_data["discriminator"]
+  end
+
   def to_s
-    "#{self.uid}" #TODO show username#discrim
+    "#{self.username}##{self.discrim}"
   end
 end
