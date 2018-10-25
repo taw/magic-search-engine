6f5f5c9fa9fe7850545f7e9096ab326dbfdbc7c2
Fenhl <fenhl@fenhl.net>
2018-10-25 14:32:39 +0000

    Fix infinite loop in Custom Modern format

--- search-engine/lib/format/format_custom_modern.rb
+++ search-engine/lib/format/format_custom_modern.rb
@@ -8,6 +8,6 @@ class FormatCustomModern < Format
   end
 
   def build_included_sets
-    Format["custom modern"].new(@time).build_included_sets - Set["ayr", "dms", "ank"]
+    Format["custom eternal"].new(@time).build_included_sets - Set["ayr", "dms", "ank"]
   end
 end
