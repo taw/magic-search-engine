86edeaae0368499a6389a10b8e2b883f8e0c61fa
Fenhl <fenhl@fenhl.net>
2018-12-01 08:25:47 +0000

    Add Custom Eternal banlist

---
+++ search-engine/lib/ban_list/custom_eternal.rb
@@ -0,0 +1,9 @@
+BanList.for_format("custom eternal") do
+  change(
+    "2018-12-01",
+    "https://discordapp.com/channels/205457071380889601/326822492222128138/518337286954287109",
+    "Treasure Room of Akhamesses" => "banned",
+    "Reev, Equinox Prophet" => "banned",
+    "Dreamheart Mox" => "banned",
+  )
+end
