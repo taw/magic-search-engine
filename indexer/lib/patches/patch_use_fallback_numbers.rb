c47271da222fd4350da522212cf74d49bc02f724
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-02-23 11:30:41 +0000

    Use multiverseid based numbers if available

--- indexer/lib/patches/patch_use_fallback_numbers.rb
+++ indexer/lib/patches/patch_use_fallback_numbers.rb
@@ -10,36 +10,45 @@ class PatchUseFallbackNumbers < Patch
 
   def get_fallback_numbers(set_code)
     path = Indexer::ROOT + "collector_numbers/#{set_code}.txt"
-    raise "Requesting MCI numbers for #{set_code} but we don't have them" unless path.exist?
+    raise "Requesting fallback numbers for #{set_code} but we don't have them" unless path.exist?
     path.readlines.map{|line|
       number, name, multiverseid = line.chomp.split("\t", 3)
-      [number, name.downcase]
-    }.group_by(&:last).transform_values{|x| x.map(&:first)}
+      [number, name.downcase, multiverseid && multiverseid.to_i]
+    }
   end
 
-  # Assume that if two Forests are 100 and 101
-  # then Forest with lower multiverse id gets 100
-  # No idea if that's correct
   def use_fallback_numbers(set_code)
-    fallback_numbers = get_fallback_numbers(set_code)
     cards = cards_by_set[set_code]
 
-    if set_code == "ced" or set_code == "cei"
-      # Not on Gatherer
-      cards.each do |card|
-        name = card["name"]
-        card["number"] = fallback_numbers[name.downcase].shift
-      end
+    if cards.all?{|c| c["multiverseid"] }
+      use_fallback_numbers_gatherer(set_code)
     else
-      mvids = cards.map{|c| [c["name"], c["multiverseid"]]}
-        .group_by(&:first)
-        .transform_values{|x| x.map(&:last).sort}
-      cards.each do |card|
-        name = card["name"]
-        rel_idx = mvids[name].index(card["multiverseid"])
-        raise unless fallback_numbers[name.downcase]
-        card["number"] = fallback_numbers[name.downcase][rel_idx]
-      end
+      use_fallback_numbers_nongatherer(set_code)
+    end
+  end
+
+  def use_fallback_numbers_gatherer(set_code)
+    cards = cards_by_set[set_code]
+    fallback_numbers = get_fallback_numbers(set_code)
+    raise unless fallback_numbers.all?{|_,_,multiverseid| multiverseid}
+
+    map = fallback_numbers.map{|id, name, multiverseid| [multiverseid, id]}.to_h
+    cards.each do |card|
+      card["number"] = map.fetch(card["multiverseid"])
+    end
+  end
+
+  # ced / cei
+  def use_fallback_numbers_nongatherer(set_code)
+    cards = cards_by_set[set_code]
+    fallback_numbers = get_fallback_numbers(set_code)
+      .group_by{|_, name, _| name}
+      .transform_values{|rows|
+        rows.map(&:first).sort_by{|n| [n.to_i, n] }
+      }
+    cards.each do |card|
+      name = card["name"]
+      card["number"] = fallback_numbers[name.downcase].shift
     end
   end
 end
