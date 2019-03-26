65f6d54c3ae4d5a6e1ec7fa9591cdf31aaf89b03
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-03-26 00:51:26 +0000

    Upgrade to jquery 3

--- frontend/app/assets/javascripts/application.js
+++ frontend/app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require turbolinks
