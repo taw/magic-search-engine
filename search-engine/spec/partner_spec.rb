b7743b6ee27ca7a0e40cb3496bb817c1653d50a9
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-04-20 06:36:16 +0100

    is:partner and has:partner queries (will be useful for BBD Sealed)

---
+++ search-engine/spec/partner_spec.rb
@@ -0,0 +1,11 @@
+describe "partner queries" do
+  include_context "db"
+
+  it "is:partner" do
+    assert_search_equal "is:partner", %Q[(o:"partner with" e:bbd) OR (o:"partner" t:legendary t:creature e:c16,cm2)]
+  end
+
+  it "has:partner" do
+    assert_search_equal "has:partner", %Q[o:"partner with" e:bbd]
+  end
+end
