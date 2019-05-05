7ecbc89952b8f80358dfbf75c2cd641a96565265
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-05-05 20:44:06 +0100

    Make deck parser resolve tags like [M10/123]

--- search-engine/spec/deck_parser_spec.rb
+++ search-engine/spec/deck_parser_spec.rb
@@ -1,5 +1,18 @@
 describe DeckParser do
   include_context "db"
+
+  def physical_by_query(query, foil=false)
+    printings = db.search(query).printings
+    raise "Ambiguous query #{query.inspect}" if printings.size != 1
+    PhysicalCard.for(printings[0], foil)
+  end
+
+  def physical_by_best(name, foil=false)
+    printing = db.cards[name].printings.min_by(&:default_sort_index)
+    raise "No such printing #{name}" unless printing
+    PhysicalCard.for(printing, foil)
+  end
+
   let(:parser) { DeckParser.new(db, text) }
   let(:deck) { parser.deck }
   let(:main_cards) { parser.main_cards }
@@ -100,18 +113,18 @@ describe DeckParser do
     end
     it do
       parser.main.should eq([
-        {name: "Lightning Bolt", count: 10, set: "M10"},
-        {name: "Lightning Bolt", count: 10, set: "4ed", number: "208"},
+        {name: "Lightning Bolt", count: 10, set_code: "M10"},
+        {name: "Lightning Bolt", count: 10, set_code: "4ed", number: "208"},
         {name: "Lightning Bolt", count: 10, foil: true},
-        {name: "Lightning Bolt", count: 10, set: "M11", foil: true},
+        {name: "Lightning Bolt", count: 10, set_code: "M11", foil: true},
         {name: "A25 Lightning Bolt", count: 10}, # FIXME
         {name: "A25 Lightning Bolt", count: 10, foil: true}, # FIXME
-        {name: "Lightning Bolt", count: 10, set: "A25", number: "141"},
-        {name: "Lightning Bolt", count: 10, set: "A25", number: "141", foil: true},
+        {name: "Lightning Bolt", count: 10, set_code: "A25", number: "141"},
+        {name: "Lightning Bolt", count: 10, set_code: "A25", number: "141", foil: true},
       ])
       parser.side.should eq([
-        {name: "Goblin Guide", count: 2, set: "ZEN", foil: true},
-        {name: "Goblin Guide", count: 1, set: "ZEN", foil: true},
+        {name: "Goblin Guide", count: 2, set_code: "ZEN", foil: true},
+        {name: "Goblin Guide", count: 1, set_code: "ZEN", foil: true},
       ])
     end
   end
@@ -126,9 +139,9 @@ describe DeckParser do
       15 Goblin Guide
       EOF
     end
-    let(:lava_spike) { PhysicalCard.for db.cards["lava spike"].printings.min_by(&:default_sort_index) }
-    let(:taiga) { PhysicalCard.for db.cards["taiga"].printings.min_by(&:default_sort_index) }
-    let(:goblin_guide) { PhysicalCard.for db.cards["goblin guide"].printings.min_by(&:default_sort_index) }
+    let(:lava_spike) { physical_by_best("lava spike") }
+    let(:taiga) { physical_by_best("taiga") }
+    let(:goblin_guide) { physical_by_best("goblin guide") }
 
     it do
       parser.main_cards.should eq [[40, lava_spike], [20, taiga]]
@@ -145,10 +158,10 @@ describe DeckParser do
       4 Assemble
       EOF
     end
-    let(:fire_ice) { PhysicalCard.for db.cards["fire"].printings.min_by(&:default_sort_index) }
-    let(:delver_of_secrets) { PhysicalCard.for db.cards["delver of secrets"].printings.min_by(&:default_sort_index) }
-    let(:thing_in_the_ice) { PhysicalCard.for db.cards["thing in the ice"].printings.min_by(&:default_sort_index) }
-    let(:assure_assemble) { PhysicalCard.for db.cards["assure"].printings.min_by(&:default_sort_index) }
+    let(:fire_ice) { physical_by_best("fire") }
+    let(:delver_of_secrets) { physical_by_best("delver of secrets") }
+    let(:thing_in_the_ice) { physical_by_best("thing in the ice") }
+    let(:assure_assemble) { physical_by_best("assure") }
 
     it do
       parser.main_cards.should eq([
@@ -170,10 +183,10 @@ describe DeckParser do
       EOF
     end
 
-    let(:lightning_bolt) { PhysicalCard.for db.cards["lightning bolt"].printings.min_by(&:default_sort_index) }
+    let(:lightning_bolt) { physical_by_best("lightning bolt") }
     let(:blue_eyes_white_dragon) { UnknownCard.new("Blue-Eyes White Dragon") }
     let(:pod_of_greed) { UnknownCard.new("Pod of Greed") }
-    let(:mountain) { PhysicalCard.for db.cards["mountain"].printings.min_by(&:default_sort_index) }
+    let(:mountain) { physical_by_best("mountain") }
 
     it do
       parser.main_cards.should eq([
@@ -197,17 +210,63 @@ describe DeckParser do
       EOF
     end
 
-    let(:tezzeret) { db.cards["tezzeret, master of the bridge"].printings.min_by(&:default_sort_index) }
-    let(:black_lotus) { db.cards["black lotus"].printings.min_by(&:default_sort_index) }
+    it do
+      parser.main_cards.should eq([
+        [1, physical_by_best("tezzeret, master of the bridge") ],
+        [2, physical_by_best("tezzeret, master of the bridge", true) ],
+        [3, physical_by_best("black lotus") ],
+        [4, physical_by_best("black lotus", true) ],
+        [11, UnknownCard.new("Pod of Greed")],
+      ])
+    end
+  end
+
+  describe "it resolves to specified set, or best available set" do
+    let(:text) do
+      <<~EOF
+      1 Birds of Paradise [LEA]
+      # Check aliases
+      2 Birds of Paradise [4e]
+      3 Birds of Paradise [5ed]
+      4 Black Lotus [m10]
+      5 Black Lotus [lea]
+      6 Black Lotus [2ed]
+      EOF
+    end
 
     it do
       parser.main_cards.should eq([
-        [1, PhysicalCard.for(tezzeret)],
-        [2, PhysicalCard.for(tezzeret, true)],
-        [3, PhysicalCard.for(black_lotus)],
-        [4, PhysicalCard.for(black_lotus, true)],
-        [5, UnknownCard.new("Pod of Greed")],
-        [6, UnknownCard.new("Pod of Greed")],
+        [1, physical_by_query("birds of paradise e:lea")],
+        [2, physical_by_query("birds of paradise e:4e")],
+        [3, physical_by_query("birds of paradise e:5e")],
+        [10, physical_by_query("black lotus e:2ed")],
+        [5, physical_by_query("black lotus e:lea")],
+      ])
+    end
+  end
+
+  describe "it resolves number if possible" do
+    let(:text) do
+      <<~EOF
+      1 Brothers Yamazaki [CHK/160a]
+      2 Brothers Yamazaki [CHK/160B]
+      3 Ice // Fire [UMA:225]
+      # This is correct number is wrong set, testing that set takes precedence
+      4 Birds of Paradise [M10/165]
+      EOF
+    end
+
+    let(:brother_a) { physical_by_query("brothers yamazaki number=160a") }
+    let(:brother_b) { physical_by_query("brothers yamazaki number=160b") }
+    let(:ice_fire) { physical_by_query("fire e:uma number=225a") }
+    let(:birds_m10) { physical_by_query("birds of paradise e:m10 number=168") }
+
+    it do
+      parser.main_cards.should eq([
+        [1, brother_a],
+        [2, brother_b],
+        [3, ice_fire],
+        [4, birds_m10],
       ])
     end
   end
