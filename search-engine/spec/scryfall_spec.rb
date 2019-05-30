8687b12ba95ed5c43ae548e2075f3e84dacea5e2
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-05-30 03:14:25 +0100

    6 more promo sets

--- search-engine/spec/scryfall_spec.rb
+++ search-engine/spec/scryfall_spec.rb
@@ -408,7 +408,7 @@ describe "Scryfall" do
   it "is_digital" do
     # scryfall includes "Gleemox" - https://scryfall.com/card/pgmx/1
     # and I have no idea what's that
-    assert_search_equal "is:digital", "e:me1 OR e:me2 OR e:me3 OR e:me4 OR e:vma OR e:tpr"
+    assert_search_equal "is:digital", "e:me1,me2,me3,me4,vma,tpr,pana"
   end
 
   it "set: is alias of e:" do
