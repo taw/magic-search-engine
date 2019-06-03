309e3e4779656ae2ab81de793bed2b41f5b8f38f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-06-03 16:06:29 +0100

    10 more sets

--- search-engine/lib/card.rb
+++ search-engine/lib/card.rb
@@ -193,7 +193,7 @@ class Card
       # "*" < "*²" < "1+*" < "2+*"
       # but let's not get anywhere near that
       case val
-      when "*", "*²", "1+*", "2+*", "7-*", "X", "∞", "?"
+      when "*", "*²", "1+*", "2+*", "7-*", "X", "∞", "?", "1d4+1"
         val
       else
         raise "Unrecognized value #{val.inspect}"
