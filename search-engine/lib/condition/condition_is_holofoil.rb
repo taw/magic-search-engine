929f67d77e4a496f2013ef46dc212321d2a5c2ff
Fenhl <fenhl@fenhl.net>
2019-06-08 17:51:03 +0000

    Don't show back faces in is:holofoil

--- search-engine/lib/condition/condition_is_holofoil.rb
+++ search-engine/lib/condition/condition_is_holofoil.rb
@@ -3,6 +3,7 @@ class ConditionIsHolofoil < ConditionSimple
     return false unless card.frame == "m15"
     return true if card.set_code == "ust" and card.rarity == "basic"
     return false unless %W[rare mythic special].include?(card.rarity)
+    return false if card.back?
     return false if card.types.include?("contraption")
     true
   end
