25df6e83a14e99bf673a32e0b4b34b1e64726619
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-06-08 20:37:37 +0100

    Fixed tests

--- search-engine/spec/holofoil_spec.rb
+++ search-engine/spec/holofoil_spec.rb
@@ -3,6 +3,6 @@ describe "is:holofoil" do
 
   it do
     # UST is exception to usual rules as full art basics have holofoil, but rare contraptions don't
-    assert_search_equal "++ is:holofoil", "++ (r>=rare or (e:ust r:basic)) frame=m15 -t:contraption"
+    assert_search_equal "++ is:holofoil", "++ (r>=rare or (e:ust r:basic)) frame=m15 -t:contraption -is:back"
   end
 end
