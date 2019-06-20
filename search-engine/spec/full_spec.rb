ede99ad1260fa6a8e4eac714dd04b8dbf3fc3d60
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-06-20 09:07:28 +0100

    CED, CEI, PLS version bumped (they have changed collector numbers) PMH1 added

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -7,7 +7,7 @@ describe "Full Database Test" do
   # indexer changes etc.
   it "stats" do
     db.number_of_cards.should eq(19778)
-    db.number_of_printings.should eq(41917)
+    db.number_of_printings.should eq(41918)
   end
 
   it "block codes" do
@@ -299,7 +299,7 @@ describe "Full Database Test" do
     # it's not totally clear what counts as "promo"
     # and different engines return different results
     # It might be a good idea to sort out edge cases someday
-    assert_count_printings "is:promo", 2794
+    assert_count_printings "is:promo", 2795
   end
 
   it "is:funny" do
