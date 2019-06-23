efba19a38178bfc848865548c05898457bc28f71
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-06-23 15:30:52 +0100

    DD2 migrated to v4.4.1, it now includes Japanese alt art Jace and Chandra

--- search-engine/spec/cli_frontend_spec.rb
+++ search-engine/spec/cli_frontend_spec.rb
@@ -42,7 +42,7 @@ describe "CLI Frontend" do
       verbose: true,
       output: <<-EOF,
         Jace Beleren {1}{u}{u}
-        [lrw dd2 pbok m10 m11 jvc ss1]
+        [lrw dd2 dd2 pbok m10 m11 jvc ss1]
         Legendary Planeswalker - Jace
         [+2]: Each player draws a card.
         [−1]: Target player draws a card.
