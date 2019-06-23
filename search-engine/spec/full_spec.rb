6187ae0f53f296e57c1cc8c2c0ff318d4e131528
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-06-23 15:51:16 +0100

    WC02 (manually hacked due to mtgjson bug, fix scheduled next release)

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -7,7 +7,7 @@ describe "Full Database Test" do
   # indexer changes etc.
   it "stats" do
     db.number_of_cards.should eq(19796)
-    db.number_of_printings.should eq(42084)
+    db.number_of_printings.should eq(42230)
   end
 
   it "block codes" do
