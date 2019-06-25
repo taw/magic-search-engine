c16b399dca3edf17cf8a50198627308942ce486d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-06-25 12:05:22 +0100

    PZ1/PZ2

--- search-engine/spec/partner_spec.rb
+++ search-engine/spec/partner_spec.rb
@@ -2,7 +2,7 @@ describe "partner queries" do
   include_context "db"
 
   it "is:partner" do
-    assert_search_equal "is:partner", %Q[(o:"partner with" e:bbd,pbbd) OR (o:"partner" t:legendary t:creature e:c16,cm2)]
+    assert_search_equal "is:partner", %Q[(o:"partner with" e:bbd,pbbd,pz2) OR (o:"partner" t:legendary t:creature e:c16,cm2,pz2)]
   end
 
   it "has:partner" do
