ae2134485302eb8ec649928ad7ce0cdf1a051c74
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-07-02 10:18:29 +0100

    Fixed test

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -7,7 +7,7 @@ describe "Full Database Test" do
   # indexer changes etc.
   it "stats" do
     db.number_of_cards.should eq(19798)
-    db.number_of_printings.should eq(43990)
+    db.number_of_printings.should eq(43952)
   end
 
   it "is:promo" do
