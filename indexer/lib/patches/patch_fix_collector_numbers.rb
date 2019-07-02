1b5b059a3b60a0704106ddead72fc89e452a5221
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-07-02 08:17:41 +0100

    UST updated to v4

--- indexer/lib/patches/patch_fix_collector_numbers.rb
+++ indexer/lib/patches/patch_fix_collector_numbers.rb
@@ -2,6 +2,7 @@
 # Also this whole file will need full rewrite for v4 migration
 class PatchFixCollectorNumbers < Patch
   def call
+    warn "This file should get removed after v4 migration is complete"
     each_set do |set|
       fix_numbers(set)
     end
@@ -15,24 +16,6 @@ class PatchFixCollectorNumbers < Patch
     cards = cards_by_set[set_code]
 
     case set_code
-    when "ust"
-      cards_with_variants = %W[3 12 41 49 54 67 82 98 103 113 145 147 165]
-      variant_counter = {}
-      cards.each do |card_data|
-        number = card_data["number"]
-        next unless cards_with_variants.include?(number)
-        variant_counter[number] = variant_counter[number] ? variant_counter[number].next : "A"
-        card_data["number"] = number + variant_counter[number]
-      end
-    when "me4"
-      # Gatherer numbers use same number for 4 alt art variants of each Urza's land
-      # add A B C D to them
-      cards.group_by{|c| c["number"]}.each do |number, variants|
-        next if variants.size == 1
-        variants.sort_by{|c| c["multiverseid"]}.each_with_index do |card, i|
-          card["number"] += "ABCD"[i]
-        end
-      end
     # These are somewhat silly orders
     when "s00"
       cards
