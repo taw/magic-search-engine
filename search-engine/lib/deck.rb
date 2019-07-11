a8cc7bcd0c956acc0bdc33bdc91e3d5f9083605e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-07-12 00:51:54 +0100

    Deck#color_identity

--- search-engine/lib/deck.rb
+++ search-engine/lib/deck.rb
@@ -72,4 +72,9 @@ class Deck
       false
     end
   end
+
+  def color_identity
+    return nil unless number_of_sideboard_cards.between?(1, 2)
+    @sideboard.map{|n,c| c.color_identity}.inject{|c1, c2| (c1.chars | c2.chars).sort.join }
+  end
 end
