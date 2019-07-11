6b0437cc13cc24a942795692a8496feeff9aeefa
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-07-12 00:00:55 +0100

    deck_card_issues for FormatCommander and FormatBrawl

--- search-engine/lib/format/format_brawl.rb
+++ search-engine/lib/format/format_brawl.rb
@@ -13,4 +13,22 @@ class FormatBrawl < FormatStandard
     end
     issues
   end
+
+  def deck_card_issues(deck)
+    issues = []
+    deck.card_counts.each do |card, name, count|
+      card_legality = legality(card)
+      case card_legality
+      when "legal", "restricted"
+        if count > 1 and not card.allowed_in_any_number?
+          issues << "Deck contains #{count} copies of #{name}, only up to 1 allowed"
+        end
+      when "banned"
+        issues << "Deck contains #{name} which is banned"
+      else
+        issues << "Deck contains #{name} which is not in the format"
+      end
+    end
+    issues
+  end
 end
