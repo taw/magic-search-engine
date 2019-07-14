cfc8ead2e1f301ace27739e97eeeb21c4a0db91b
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-07-14 01:25:00 +0100

    Rewriting sort spec to actually show what it expects to happen instead of being blind snapshot

--- search-engine/spec/kld_spec.rb
+++ search-engine/spec/kld_spec.rb
@@ -5,26 +5,4 @@ describe "Kaladesh" do
     assert_search_results "pow=10", "Demolition Stomper", "Metalwork Colossus"
     assert_search_results "tou=7", "Accomplished Automaton", "Demolition Stomper"
   end
-
-  it "sort:pow" do
-    assert_search_results_ordered "r:mythic t:artifact sort:pow",
-      "Combustible Gearhulk",          # 6
-      "Skysovereign, Consul Flagship", # 6 vehicle
-      "Noxious Gearhulk",              # 5
-      "Torrential Gearhulk",           # 5
-      "Cataclysmic Gearhulk",          # 4
-      "Verdurous Gearhulk",            # 4
-      "Aetherworks Marvel"             # nil
-  end
-
-  it "sort:tou" do
-     assert_search_results_ordered "r:mythic t:artifact sort:tou",
-       "Combustible Gearhulk",          # 6
-       "Torrential Gearhulk",           # 6
-       "Cataclysmic Gearhulk",          # 5
-       "Skysovereign, Consul Flagship", # 5 vehicle
-       "Noxious Gearhulk",              # 4
-       "Verdurous Gearhulk",            # 4
-       "Aetherworks Marvel"             # nil
-  end
 end
