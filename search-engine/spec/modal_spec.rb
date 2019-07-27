5a93928cfe799bb333aa0eee79299522c74a1c84
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-07-27 22:50:19 +0100

    Support for is:modal

---
+++ search-engine/spec/modal_spec.rb
@@ -0,0 +1,8 @@
+describe "Modal cards" do
+  include_context "db"
+
+  it "is:modal" do
+    assert_search_equal "is:modal", %Q[o:/(choose|opponent chooses) .*\n•/]
+    assert_search_equal "not:modal", "-(is:modal)"
+  end
+end
