96730008c5fb4529dff5dddb12924577aa5f0f83
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-08-03 11:09:43 +0100

    A few tweaks to syntax help

--- frontend/app/helpers/help_helper.rb
+++ frontend/app/helpers/help_helper.rb
@@ -1,5 +1,5 @@
 module HelpHelper
   def search_help(query, explanation)
-    "<li>#{explanation} - #{link_to_search(query){ query }}</li>".html_safe
+    "<li>#{format_mana_symbols_in_text explanation} - #{link_to_search(query){ query }}</li>".html_safe
   end
 end
