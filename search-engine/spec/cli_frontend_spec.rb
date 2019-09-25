ed4e7aefa0a5959a1ff066adfcede88673cf7be9
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-09-25 16:30:27 +0100

    mtgjson update

--- search-engine/spec/cli_frontend_spec.rb
+++ search-engine/spec/cli_frontend_spec.rb
@@ -56,7 +56,7 @@ describe "CLI Frontend" do
       verbose: true,
       output: <<-EOF,
         Siege Rhino {1}{w}{b}{g}
-        [pktk ktk cp3 prm]
+        [ktk pktk cp3 prm]
         Creature - Rhino
         Trample
         When Siege Rhino enters the battlefield, each opponent loses 3 life and you gain 3 life.
