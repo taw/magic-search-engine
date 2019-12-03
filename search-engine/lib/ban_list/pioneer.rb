5cd772891daa2f317bb590ffd4d2457759fd3413
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-12-03 09:01:46 +0000

    Pioneer bans

--- search-engine/lib/ban_list/pioneer.rb
+++ search-engine/lib/ban_list/pioneer.rb
@@ -21,4 +21,12 @@ BanList.for_format("pioneer") do
     "https://magic.wizards.com/en/articles/archive/news/november-11-2019-pioneer-banned-announcement",
     "Veil of Summer" => "banned",
   )
+
+  change(
+    "2019-12-03",
+    "https://magic.wizards.com/en/articles/archive/news/december-2-2019-pioneer-banned-announcement",
+    "Once Upon a Time" => "banned",
+    "Field of the Dead" => "banned",
+    "Smuggler's Copter" => "banned",
+  )
 end
