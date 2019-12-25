6328bfe4d394bda3c14af46179a8965c35ed1c1d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-12-25 20:45:43 +0000

    Refactored list of ability word like things

--- search-engine/spec/ability_word_spec.rb
+++ search-engine/spec/ability_word_spec.rb
@@ -7,11 +7,24 @@ describe "Ability Word Regexp" do
 
     # If this list ever changes, this test should fail
 
+    other_phrases = [
+      "An opponent chooses one",
+      "Choose one or both",
+      "Choose one or more",
+      "Choose one",
+      "Choose two",
+      "DCI ruling",
+      "Forecast",
+      "I",
+      "II",
+      "III",
+    ]
+
     card_texts = db.cards.values.map(&:text)
     dash_prefixes = card_texts.map{|t| t.scan(/^([a-zA-Z' ]+) —/)}.flatten.uniq.sort
     ability_words = card_texts.map{|t| t.scan(Card::ABILITY_WORD_RX)}.flatten.uniq.sort
     (ability_words - dash_prefixes).should eq([])
-    (dash_prefixes - ability_words).should match_array(["An opponent chooses one", "Choose one", "Choose one or both", "Choose one or more", "Choose two", "Forecast", "DCI ruling", "I", "II", "III"])
+    (dash_prefixes - ability_words).should match_array(other_phrases)
     ability_words.should match_array(Card::ABILITY_WORD_LIST)
   end
 end
