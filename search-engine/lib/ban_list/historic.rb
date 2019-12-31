16b32c61abe8ba04ca6212d0e38b2e503713e6de
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-12-31 04:53:26 +0000

    FormatHistoric

---
+++ search-engine/lib/ban_list/historic.rb
@@ -0,0 +1,10 @@
+BanList.for_format("historic") do
+  change(
+    "2019-12-10",
+    "https://magic.wizards.com/en/articles/archive/magic-digital/historic-suspension-announcement-2019-12-10",
+    "Once Upon a Time" => "banned",
+    "Field of the Dead" => "banned",
+    "Veil of Summer" => "banned",
+    "Oko, Thief of Crowns" => "banned",
+  )
+end
