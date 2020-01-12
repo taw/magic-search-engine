7217cb3198341c9077086f84379c959ae2b42a45
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-01-12 13:29:24 +0000

    HTR18 is now officially funny

--- indexer/lib/patches/patch_funny.rb
+++ indexer/lib/patches/patch_funny.rb
@@ -2,7 +2,7 @@ class PatchFunny < Patch
   def call
     errata_sets = @sets.select{|set| set["type"] == "errata"}.map{|set| set["code"].downcase}
     # With so many sets, it's hard to keep this list maintained
-    funny_sets = %W[unh ugl pcel hho parl prel ust pust ppc1 htr htr17 pal04 h17 j17 tbth tdag tfth thp1 thp2 thp3 ptg cmb1]
+    funny_sets = %W[unh ugl pcel hho parl prel ust pust ppc1 htr htr17 pal04 h17 j17 tbth tdag tfth thp1 thp2 thp3 ptg cmb1 htr18]
     each_card do |name, printings|
       funny = printings.all?{|card| funny_sets.include?(card["set_code"]) || errata_sets.include?(card["set_code"]) }
 
