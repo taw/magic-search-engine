2f6b397fd0e85b67e57ccb77c414bd29b4881a9f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-02-27 05:45:24 +0000

    Automate set prioritization

--- indexer/lib/indexer.rb
+++ indexer/lib/indexer.rb
@@ -192,6 +192,7 @@ class Indexer
         "name",
         "releaseDate",
         "type",
+        "meta",
       ).merge(
         "mci_code" => set_data["magicCardsInfoCode"],
         "gatherer_code" => set_data["gathererCode"],
--- indexer/lib/patches/patch_assign_priorities_to_sets.rb
+++ indexer/lib/patches/patch_assign_priorities_to_sets.rb
@@ -1,5 +1,7 @@
 class PatchAssignPrioritiesToSets < Patch
   def call
+    @versions = [nil, *@sets.map{|x| x.dig("meta", "date") }.uniq.sort]
+
     each_set do |set|
       set["priority"] = priority(set)
     end
@@ -9,18 +11,17 @@ class PatchAssignPrioritiesToSets < Patch
 
   def priority(set)
     # Errata sets are just a way to apply Oracle erratas without creating any cards
-    return 1000 if set["types"].include?("errata")
+    return 2000 if set["types"].include?("errata")
 
     # Give all unlisted custom sets max priority
     # If you want to customize priority between different custom sets, just list them explicitly
-    return 100 if set["custom"]
+    return 1000 if set["custom"]
 
-    # case set["code"]
-    # when "pwar", "prm", "eld", "celd"
-    #   return 5
-    # end
-
-    return 10 if set["v4"]
+    if set["v4"]
+      # Automatically make more recent sets take priority
+      date = set.dig("meta", "date")
+      return 10 + @versions.index(date)
+    end
     # Default priority for everything else
     0
   end
