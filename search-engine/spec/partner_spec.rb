331693f216ebded62359625f622df3ac15910482
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-04-20 17:06:18 +0100

    C20 decks

--- search-engine/spec/partner_spec.rb
+++ search-engine/spec/partner_spec.rb
@@ -2,10 +2,10 @@ describe "partner queries" do
   include_context "db"
 
   it "is:partner" do
-    assert_search_equal "is:partner", %Q[(o:"partner with" e:bbd,pbbd,pz2) OR (o:"partner" t:legendary t:creature e:c16,cm2,pz2)]
+    assert_search_equal "is:partner", %Q[(o:"partner with" e:bbd,pbbd,pz2,c20) OR (o:"partner" t:legendary t:creature e:c16,cm2,pz2)]
   end
 
   it "has:partner" do
-    assert_search_equal "has:partner", %Q[o:"partner with" e:bbd,pbbd]
+    assert_search_equal "has:partner", %Q[o:"partner with" e:bbd,pbbd,c20]
   end
 end
