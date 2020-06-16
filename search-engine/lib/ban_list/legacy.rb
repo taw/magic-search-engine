b8ae36d551e815889853cf64b14329f990c1b735
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-06-16 02:05:53 +0100

    Partial update of "racist cards" ban. I need to actually figure out which formats they were legal in the first place.

--- search-engine/lib/ban_list/legacy.rb
+++ search-engine/lib/ban_list/legacy.rb
@@ -317,5 +317,17 @@ BanList.for_format("legacy") do
     "https://magic.wizards.com/en/articles/archive/news/may-18-2020-banned-and-restricted-announcement",
     "Lurrus of the Dream-Den" => "banned",
     "Zirda, the Dawnwaker" => "banned",
-    )
+  )
+
+  change(
+    "2020-06-10",
+    "https://magic.wizards.com/en/articles/archive/news/depictions-racism-magic-2020-06-10",
+    "Cleanse" => "banned",
+    "Crusade" => "banned",
+    "Imprison" => "banned",
+    "Invoke Prejudice" => "banned",
+    "Jihad" => "banned",
+    "Pradesh Gypsies" => "banned",
+    "Stone-Throwing Devils" => "banned",
+  )
 end
