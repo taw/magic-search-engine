e5abfbf48f923b6a27af92096cc7e4669b285d1d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-17 13:51:10 +0100

    AKR

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -7,7 +7,7 @@ describe "Full Database Test" do
   # for same sets, indexer changes etc.
   it "stats" do
     db.number_of_cards.should eq(21278)
-    db.number_of_printings.should eq(51145)
+    db.number_of_printings.should eq(51509)
   end
 
   # I'm not even sure what good this test does, delete?
