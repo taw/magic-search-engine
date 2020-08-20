dd28d6f0c4cb13d3bff1e973bf7b2f1ffe8d35d2
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-20 15:13:14 +0100

    SLD update

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -7,7 +7,7 @@ describe "Full Database Test" do
   # for same sets, indexer changes etc.
   it "stats" do
     db.number_of_cards.should eq(21278)
-    db.number_of_printings.should eq(51509)
+    db.number_of_printings.should eq(51514)
   end
 
   # I'm not even sure what good this test does, delete?
@@ -15,7 +15,7 @@ describe "Full Database Test" do
     # it's not totally clear what counts as "promo"
     # and different engines return different results
     # It might be a good idea to sort out edge cases someday
-    assert_count_printings "is:promo", 5019
+    assert_count_printings "is:promo", 5024
   end
 
   it "block codes" do
