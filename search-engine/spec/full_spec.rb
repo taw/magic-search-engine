f046e0402ce556b0691c9dcb4df006a7df5a9806
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-10-08 13:35:12 +0100

    Reverted inclusion of TSR, 3/121 spoiler is useless

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -7,7 +7,7 @@ describe "Full Database Test" do
   # for same sets, indexer changes etc.
   it "stats" do
     db.number_of_cards.should eq(21585)
-    db.number_of_printings.should eq(52412)
+    db.number_of_printings.should eq(52409)
   end
 
   # I'm not even sure what good this test does, delete?
@@ -241,7 +241,6 @@ describe "Full Database Test" do
       "Lotus Petal",
       "Meekstone",
       "Ornithopter",
-      "Relentless Rats",
       "Spell Pierce"
   end
 
