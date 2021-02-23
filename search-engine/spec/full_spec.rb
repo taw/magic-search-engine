6ae08b7b4db71f5c7291f8118087974d6387d855
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-02-23 15:29:25 +0000

    mtgjson update

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -7,7 +7,7 @@ describe "Full Database Test" do
   # for same sets, indexer changes etc.
   it "stats" do
     db.number_of_cards.should eq(22072)
-    db.number_of_printings.should eq(54696)
+    db.number_of_printings.should eq(54698)
   end
 
   # I'm not even sure what good this test does, delete?
