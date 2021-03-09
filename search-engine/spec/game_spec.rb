2e7f4f087482eaac230469fd2f6a2baccda8be0b
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-03-09 14:17:06 +0000

    HA4 added. Rarity special is back. CMR packs do not do Prismatic Piper correctly, they need fixing

--- search-engine/spec/game_spec.rb
+++ search-engine/spec/game_spec.rb
@@ -2,7 +2,7 @@ describe "Game queries" do
   include_context "db"
 
   it "is_digital" do
-    assert_search_equal "is:digital", "e:me1,me2,me3,me4,vma,tpr,pana,pmoa,td0,td2,ana,pz1,pz2,prm,ha1,ha2,ha3,xana,past,pmic,psdg,ajmp,akr,anb,oana,klr"
+    assert_search_equal "is:digital", "e:me1,me2,me3,me4,vma,tpr,pana,pmoa,td0,td2,ana,pz1,pz2,prm,ha1,ha2,ha3,ha4,xana,past,pmic,psdg,ajmp,akr,anb,oana,klr"
   end
 
   it "is:paper" do
