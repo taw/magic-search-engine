edc9c8b964998fe6a350f3362c0d13b0901e136d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-03-27 14:09:19 +0000

    Open all spack types in random tests including variant types

--- search-engine/spec/pack_random_spec.rb
+++ search-engine/spec/pack_random_spec.rb
@@ -3,9 +3,7 @@ describe "Opening packs smoke test" do
   let(:pack_factory) { PackFactory.new(db) }
 
   it "Opening packs should return something" do
-    db.sets.each do |set_code, set|
-      pack = pack_factory.for(set_code)
-      next unless pack
+    db.supported_booster_types.each do |code, pack|
       cards = pack.open
       cards.should be_a(Array)
       cards.each do |card|
