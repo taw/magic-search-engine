57603105a0a84cc017a3e1e10f72bf1d00dcf8ed
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-03-30 11:58:39 +0100

    STA added

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -6,8 +6,8 @@ describe "Full Database Test" do
   # by changes which are not expected to, like updating to new mtgjson data
   # for same sets, indexer changes etc.
   it "stats" do
-    db.number_of_cards.should eq(22072)
-    db.number_of_printings.should eq(55078)
+    db.number_of_cards.should eq(22073)
+    db.number_of_printings.should eq(55205)
   end
 
   # I'm not even sure what good this test does, delete?
@@ -15,7 +15,7 @@ describe "Full Database Test" do
     # it's not totally clear what counts as "promo"
     # and different engines return different results
     # It might be a good idea to sort out edge cases someday
-    assert_count_printings "is:promo", 5365
+    assert_count_printings "is:promo", 5366
   end
 
   it "block codes" do
