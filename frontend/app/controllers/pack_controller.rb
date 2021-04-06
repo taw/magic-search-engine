62955189cc4db7eba477a4130a71b0d7ff21f072
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-04-06 12:38:07 +0100

    PackController#show (placeholder for now)

--- frontend/app/controllers/pack_controller.rb
+++ frontend/app/controllers/pack_controller.rb
@@ -3,4 +3,24 @@ class PackController < ApplicationController
     @booster_types = $CardDatabase.supported_booster_types
     @title = "Packs"
   end
+
+  def show
+    set_code, variant = params.require(:id).downcase.split("-", 2)
+
+    unless $CardDatabase.sets[set_code]
+      render_404
+      return
+    end
+
+    factory = PackFactory.new($CardDatabase)
+    @pack = factory.for(set_code, variant)
+
+    unless @pack
+      render_404
+      return
+    end
+
+    # ...
+  end
+
 end
