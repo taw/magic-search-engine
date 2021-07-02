d1dddedb04ea384b998a44d2b16f1bff79560073
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-07-02 07:18:49 +0100

    Add some features to related:

--- search-engine/lib/condition/condition_related.rb
+++ search-engine/lib/condition/condition_related.rb
@@ -8,7 +8,7 @@ class ConditionRelated < Condition
     @cond.search(db).each do |card|
       next unless card.related
       card.related.each do |related_name|
-        related_card = db.cards[related_name.downcase]
+        related_card = db.cards[related_name.downcase.normalize_accents]
         next unless related_card # Not supposed to happen, but related is just a regexp
         related_card.printings.each do |printing|
           results << printing
