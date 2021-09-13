67793bca7e67614ec22eabbc143f5d0966fe9059
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-09-13 21:51:02 +0100

    Support for quoting in P/T queries

--- search-engine/spec/full_spec.rb
+++ search-engine/spec/full_spec.rb
@@ -214,42 +214,6 @@ describe "Full Database Test" do
       "Spell Pierce"
   end
 
-  it "pow:special" do
-    assert_search_equal "pow=1+*", "pow=*+1"
-    assert_search_include "pow=*", "Krovikan Mist"
-    assert_search_results "pow=1+*",
-      "Allosaurus Rider",
-      "Gaea's Avenger",
-      "Haunting Apparition",
-      "Lost Order of Jarkeld",
-      "Mwonvuli Ooze",
-      "Nighthawk Scavenger"
-    assert_search_results "pow=2+*",
-      "Angry Mob", "Aysen Crusader"
-    assert_search_equal "pow>*", "pow>=1+*"
-    assert_search_equal "pow>1+*", "pow>=2+*"
-    assert_search_equal "pow>1+*", "pow=2+*"
-    assert_search_equal "pow=*2", "pow=*²"
-    assert_search_results "pow=*2",
-      "S.N.O.T."
-  end
-
-  it "loy:special" do
-    assert_search_results "loy=0", "Jeska, Thrice Reborn", "Dakkon, Shadow Slayer"
-    assert_search_equal "loy=x", "loy=X"
-    assert_search_results "loy=x", "Nissa, Steward of Elements"
-  end
-
-  it "tou:special" do
-    # Mostly same as power except 7-*
-    assert_search_results "tou=7-*", "Shapeshifter"
-    assert_search_results "tou>8-*"
-    assert_search_results "tou>2-*", "Shapeshifter"
-    assert_search_results "tou>8-*"
-    assert_search_results "tou<=8-*", "Shapeshifter"
-    assert_search_results "tou<=2-*"
-  end
-
   it "is:funny" do
     assert_search_results "abyss is:funny", "Zzzyxas's Abyss"
     assert_search_results "abyss not:funny",
