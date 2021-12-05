3a868aaa0762bf7c82457e447acb47b316ae0685
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-12-05 21:10:30 +0000

    Spec fix

--- search-engine/spec/partner_spec.rb
+++ search-engine/spec/partner_spec.rb
@@ -2,10 +2,10 @@ describe "partner queries" do
   include_context "db"
 
   it "is:partner" do
-    assert_search_equal "is:partner -e:prm", %[(o:"partner with" e:bbd,pbbd,pz2,c20,plist,cmr,voc) OR (o:"partner" t:legendary t:creature e:c16,cm2,pz2,plist,cmr,htr18,khc) or (o:"partner" t:legendary t:planeswalker e:cmr)]
+    assert_search_equal "is:partner -e:prm", %[(o:"partner with" e:bbd,pbbd,pz2,c20,plist,cmr,voc,sld) OR (o:"partner" t:legendary t:creature e:c16,cm2,pz2,plist,cmr,htr18,khc) or (o:"partner" t:legendary t:planeswalker e:cmr)]
   end
 
   it "has:partner" do
-    assert_search_equal "has:partner", %[o:"partner with" e:bbd,pbbd,c20,plist,cmr,voc]
+    assert_search_equal "has:partner", %[o:"partner with" e:bbd,pbbd,c20,plist,cmr,voc,sld]
   end
 end
