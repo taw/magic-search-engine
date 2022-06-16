71a01e2d8cc8911f110f1d0a1b117b579bfa320d
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-06-16 13:02:19 +0100

    Made routes less bot-discoverable

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Temporary
-  get "card/api/search" => "api#search"
-  get "card/api/:set/:id" => "api#show"
-  get "card/api/:set/:id/:name" => "api#show"
+  get "cardapi/search" => "api#search"
+  get "cardapi/:set/:id" => "api#show"
+  get "cardapi/:set/:id/:name" => "api#show"
 end
