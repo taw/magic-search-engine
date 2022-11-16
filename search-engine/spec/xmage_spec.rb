f63cb1b40890013a8b0b4a34250ba198753ce8cd
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-11-16 18:02:52 +0000

    Updated XMage card list and fixed spelling of Tura

--- search-engine/spec/xmage_spec.rb
+++ search-engine/spec/xmage_spec.rb
@@ -4,7 +4,7 @@ describe "XMage" do
   it "has almost all Modern legal cards" do
     # st:modern to ignore reprints in promo sets
     # Most recent sets often come to XMage late or in parts, so this -e: clause needs periodic updating
-    assert_search_results "f:modern (st:std or st:modern) -in:xmage -e:mid,vow,neo,snc,dmu,bro",
+    assert_search_results "f:modern (st:std or st:modern) -in:xmage -e:bro",
       # mutate from graveyard
       "Brokkos, Apex of Forever",
       # text change
@@ -12,14 +12,6 @@ describe "XMage" do
       "Mind Bend",
       "Spectral Shift",
       "Swirl the Mists",
-      "Trait Doctoring",
-      # AFR dice roll triggers
-      "Barbarian Class",
-      "Brazen Dwarf",
-      "Critical Hit",
-      "Farideh, Devil's Chosen",
-      "Feywild Trickster",
-      "Pixie Guide",
-      "Xanathar, Guild Kingpin"
+      "Trait Doctoring"
   end
 end
