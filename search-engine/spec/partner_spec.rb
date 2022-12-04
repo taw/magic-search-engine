f83ca6d58d02aa70329110d005d70170831ad1de
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-12-04 05:51:32 +0000

    Fixed what counts as promo set and fixed one failing spec

--- search-engine/spec/partner_spec.rb
+++ search-engine/spec/partner_spec.rb
@@ -2,10 +2,10 @@ describe "partner queries" do
   include_context "db"
 
   it "is:partner" do
-    assert_search_equal "is:partner -e:prm,plist", %[(o:"partner with" e:bbd,pbbd,pz2,c20,cmr,voc,sld,clb) OR (o:"partner" t:legendary t:creature e:c16,cm2,pz2,cmr,htr18,khc,sld,nec,phed,unf,brc) or (o:"partner" t:legendary t:planeswalker e:cmr)]
+    assert_search_equal "is:partner -is:promo", %[(o:"partner with" e:bbd,c20,cmr,voc,clb) OR (o:"partner" t:legendary t:creature e:c16,cm2,cmr,htr18,khc,nec,unf,brc) or (o:"partner" t:legendary t:planeswalker e:cmr)]
   end
 
   it "has:partner" do
-    assert_search_equal "has:partner -e:prm,plist", %[o:"partner with" e:bbd,pbbd,c20,cmr,voc,sld,clb]
+    assert_search_equal "has:partner -is:promo", %[o:"partner with" e:bbd,c20,cmr,voc,clb]
   end
 end
