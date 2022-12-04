d81cff27be90dfaf7e7e74344cb6d46da0a78028
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-12-04 18:28:32 +0000

    PBRO update

--- indexer/lib/patches/patch_multipart.rb
+++ indexer/lib/patches/patch_multipart.rb
@@ -21,7 +21,37 @@ class PatchMultipart < Patch
     matches_by_number = from_same_set.select{|c| c["number"].sub(/[ab]\z/, "") == number.sub(/[ab]\z/, "")}
     return matches_by_number[0] if matches_by_number.size == 1
 
-    # And then there's meld cards in PBRO...
-    raise "Can't resolve match for #{other_name} for #{card_name} [#{set_code}/#{number}]"
+    # And then there's meld cards in PBRO with a lot of meld cards
+    special = {
+      "pbro" => {
+        # Mishra
+        ["Phyrexian Dragon Engine", "163ap"] => {"Mishra, Lost to Phyrexia" => "163bp"},
+        ["Phyrexian Dragon Engine", "163as"] => {"Mishra, Lost to Phyrexia" => "163bs"},
+        ["Mishra, Lost to Phyrexia", "163bp"] => {"Mishra, Claimed by Gix" => "216p", "Phyrexian Dragon Engine" => "163ap"},
+        ["Mishra, Lost to Phyrexia", "163bs"] => {"Mishra, Claimed by Gix" => "216s", "Phyrexian Dragon Engine" => "163as"},
+        ["Mishra, Claimed by Gix", "216p"] => {"Mishra, Lost to Phyrexia" => "163bp"},
+        ["Mishra, Claimed by Gix", "216s"] => {"Mishra, Lost to Phyrexia" => "163bs"},
+        # Urza
+        ["Urza, Lord Protector", "225p"] => {"Urza, Planeswalker" => "238bp"},
+        ["Urza, Lord Protector", "225s"] => {"Urza, Planeswalker" => "238bs"},
+        ["The Mightstone and Weakstone", "238ap"] => {"Urza, Planeswalker" => "238bp"},
+        ["The Mightstone and Weakstone", "238as"] => {"Urza, Planeswalker" => "238bs"},
+        ["Urza, Planeswalker", "238bp"] => {"The Mightstone and Weakstone" => "238ap", "Urza, Lord Protector" => "225p"},
+        ["Urza, Planeswalker", "238bs"] => {"The Mightstone and Weakstone" => "238as", "Urza, Lord Protector" => "225s"},
+        # Titania
+        ["Titania, Voice of Gaea", "193p"] => {"Titania, Gaea Incarnate" => "256bp"},
+        ["Titania, Voice of Gaea", "193s"] => {"Titania, Gaea Incarnate" => "256bs"},
+        ["Argoth, Sanctum of Nature", "256ap"] => {"Titania, Gaea Incarnate" => "256bp"},
+        ["Argoth, Sanctum of Nature", "256as"] => {"Titania, Gaea Incarnate" => "256bs"},
+        ["Titania, Gaea Incarnate", "256bp"] => {"Titania, Voice of Gaea" => "193p", "Argoth, Sanctum of Nature" => "256ap"},
+        ["Titania, Gaea Incarnate", "256bs"] => {"Titania, Voice of Gaea" => "193s", "Argoth, Sanctum of Nature" => "256as"},
+      },
+    }
+    other_number = special.dig(set_code, [card_name, number], other_name)
+    if other_number
+      other = from_same_set.find{|c| c["number"] == other_number }
+      return other if other
+    end
+    raise "Can't resolve match for #{other_name} for #{card_name} [#{set_code}/#{number}]. Candidates are #{from_same_set.map{|c| c["number"]}.join(", ")}"
   end
 end
