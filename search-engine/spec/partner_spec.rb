d7cff3ff7e10c7f2561e78f1d2cef8797b152407
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-01-28 00:00:45 +0000

    A few more spec fixes. Full ONE/ONC support will only be possible once mtgjson stabilizes

--- search-engine/spec/partner_spec.rb
+++ search-engine/spec/partner_spec.rb
@@ -2,7 +2,7 @@ describe "partner queries" do
   include_context "db"
 
   it "is:partner" do
-    assert_search_equal "is:partner -is:promo", %[(o:"partner with" e:bbd,c20,cmr,voc,clb) OR (o:"partner" t:legendary t:creature e:c16,cm2,cmr,htr18,khc,nec,unf,brc) or (o:"partner" t:legendary t:planeswalker e:cmr)]
+    assert_search_equal "is:partner -is:promo", %[(o:"partner with" e:bbd,c20,cmr,voc,clb) OR (o:"partner" t:legendary t:creature e:c16,cm2,cmr,htr18,khc,nec,unf,brc,onc) or (o:"partner" t:legendary t:planeswalker e:cmr)]
   end
 
   it "has:partner" do
