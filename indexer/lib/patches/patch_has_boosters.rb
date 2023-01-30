90c947b9190a8b0decd6b8c0bfb21b0e1ecaf4ba
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-01-30 21:46:36 +0000

    ONE boosters

--- indexer/lib/patches/patch_has_boosters.rb
+++ indexer/lib/patches/patch_has_boosters.rb
@@ -7,7 +7,7 @@ class PatchHasBoosters < Patch
   # This just needs to list sets that didn't get to mtgjson yet
   def new_sets_with_boosters
     %W[
-      30a
+      one
     ]
   end
 
@@ -35,6 +35,7 @@ class PatchHasBoosters < Patch
       snc
       dmu
       bro
+      one
     ]
   end
 
--- search-engine/spec/booster_spec.rb
+++ search-engine/spec/booster_spec.rb
@@ -149,6 +149,8 @@ describe "is:booster" do
         assert_search_equal "e:#{set_code} is:booster", "e:#{set_code} number<=249"
       when "dmr"
         assert_search_equal "e:#{set_code} is:booster", "e:#{set_code} (number<=261 or (number>=402 number<=411))"
+      when "one"
+        assert_search_equal "e:#{set_code} is:booster", "e:#{set_code} number<=271"
       else
         if set.has_boosters? or set.in_other_boosters?
           assert_search_equal "e:#{set_code} is:booster", "e:#{set_code} -number:/†|s/"
