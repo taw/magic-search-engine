e2847bf4609781426110c610884d6635f91758c4
Axxroytovu <samzimmerman@me.com>
2023-03-26 16:09:51 -0700

    Attempt to limit the number of possible sets for booster fun treatments. Untested

--- search-engine/lib/condition/condition_is_boosterfun.rb
+++ search-engine/lib/condition/condition_is_boosterfun.rb
@@ -1,7 +1,10 @@
 class ConditionIsBoosterfun < ConditionSimple
   def match?(card)
+    valid_sets = ["eld", "thb", "iko", "m21", "znr", "khm", "stx", "afr", "mid", "vow", "neo", "snc", "dmu", "bro", "one", "2xm", "mh2", "clb", "cmr", "2x2", "dmr", "unf", "bot"]
+    return false unless valid_sets.include?(card.printing.set)
     return false if card.foiling == "foilonly"
-    card.frame_effects.include?("showcase") or card.border.include?("borderless")
+	return false if card.set.types.include?("promo")
+    card.frame_effects.include?("showcase") or card.border.include?("borderless") or card.frame_effects.include?("inverted") or card.frame.include?("old")
   end
 
   def to_s
