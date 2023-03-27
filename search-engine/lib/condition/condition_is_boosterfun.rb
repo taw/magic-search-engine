c0f918baa2e47b7f2897caf8fdb4bd65ee045ad8
Axxroytovu <samzimmerman@me.com>
2023-03-27 15:23:17 -0700

    Redesign to use Condition instead of ConditionSimple Accounts for Ikoria Godzilla treatments

--- search-engine/lib/condition/condition_is_boosterfun.rb
+++ search-engine/lib/condition/condition_is_boosterfun.rb
@@ -1,10 +1,25 @@
-class ConditionIsBoosterfun < ConditionSimple
-  def match?(card)
+class ConditionIsBoosterfun < Condition
+  def search(db)
     valid_sets = ["eld", "thb", "iko", "m21", "znr", "khm", "stx", "afr", "mid", "vow", "neo", "snc", "dmu", "bro", "one", "2xm", "mh2", "clb", "cmr", "2x2", "dmr", "unf", "bot"]
-    return false unless valid_sets.include?(card.printing.set)
-    return false if card.foiling == "foilonly"
-	return false if card.set.types.include?("promo")
-    card.frame_effects.include?("showcase") or card.border.include?("borderless") or card.frame_effects.include?("inverted") or card.frame.include?("old")
+	results = Set[]
+    db.printings.each do |card_printing|
+	  if valid_sets.include?(card_printing.set.code)
+	    unless card_printing.foiling == "foilonly"
+	      unless card_printing.types.include?("token") or card_printing.types.include?("attraction")
+		    if card_printing.set.code == "iko"
+			  if card_printing.frame_effects.include?("showcase")
+			    results << card_printing
+			  elsif card_printing.types.include?("planeswalker") and card_printing.border.include?("borderless")
+			    results << card_printing
+			  end
+		    elsif card_printing.frame_effects.include?("showcase") or card_printing.border.include?("borderless") or card_printing.frame == ("old")
+		      results << card_printing
+            end
+	      end
+	    end
+	  end
+	end
+	results
   end
 
   def to_s
