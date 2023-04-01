c8780f92a590412564b2cc274bd614c22ef1bb5b
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-04-01 09:13:37 +0100

    Moved pack option resolution to indexer

--- booster_indexer/lib/booster_indexer.rb
+++ booster_indexer/lib/booster_indexer.rb
@@ -6,7 +6,7 @@ require "yaml"
 class BoosterIndexer
   ROOT = Pathname(__dir__).parent.parent
   BOOSTER_DATA_ROOT = ROOT + "data/boosters"
-  BOOSTER_INDEX_PATH = INDEX_ROOT + "index/booster_index.json"
+  BOOSTER_INDEX_PATH = ROOT + "index/booster_index.json"
 
   def initialize
     @common = nil
@@ -24,9 +24,54 @@ class BoosterIndexer
     end
   end
 
+  def merge_pack_parts(part1, part2)
+    result = part1.dup
+    part2.each do |k, v|
+      result[k] ||= 0
+      result[k] += v
+    end
+    result
+  end
+
+  def resolve_option_combinations(pack_data)
+    options = [[{}, pack_data.delete("chance") || 1]]
+    pack_data.each do |name, count|
+      if count.is_a?(Integer)
+        options = options.map{|o,c|
+          [merge_pack_parts(o, {name => count}), c]
+        }
+      elsif count.is_a?(Array)
+        merge_options = count.map{|m| [m, m.delete("chance")]}
+        options = options.flat_map{|o1, c1|
+          merge_options.map{|o2, c2|
+            [merge_pack_parts(o1, o2), c1 * c2]
+          }
+        }
+      else
+        raise "Unknown pack count type #{count.class}"
+      end
+    end
+    options
+  end
+
+  def process_booster(name, data)
+    packs = data.delete("packs") || []
+    packs << data.delete("pack") if data.key?("pack")
+    raise "Booster #{name} has no packs" if packs.empty?
+    packs = packs.flat_map{|pack_data| resolve_option_combinations(pack_data)}
+    gcd = packs.map(&:last).reduce(:gcd)
+    data["packs"] = packs.map{|pack_data, chance| [pack_data, chance / gcd]}
+  end
+
+  def process_data
+    @boosters.each do |name, data|
+      process_booster(name, data)
+    end
+  end
+
   def call
     load_data
-    # process data
+    process_data
     BOOSTER_INDEX_PATH.write({
       "common" => @common,
       "boosters" => @boosters
--- search-engine/lib/pack_factory_yaml.rb
+++ search-engine/lib/pack_factory_yaml.rb
@@ -61,59 +61,11 @@ class PackFactoryYaml
     sheet
   end
 
-  def merge_pack_parts(part1, part2)
-    result = part1.dup
-    part2.each do |k, v|
-      result[k] ||= 0
-      result[k] += v
-    end
-    result
-  end
-
   def build_simple_pack(pack_data, sheets)
     Pack.new(pack_data.map{|name, count|
       sheet = sheets[name] or raise "Can't build sheet #{name}"
       [sheet, count]
     }.to_h)
-  end
-
-  def resolve_option_combinations(pack_data)
-    options = [[{}, 1]]
-    pack_data.each do |name, count|
-      if count.is_a?(Integer)
-        options = options.map{|o,c|
-          [merge_pack_parts(o, {name => count}), c]
-        }
-      elsif count.is_a?(Array)
-        merge_options = count.map{|m| [m, m.delete("chance")]}
-        options = options.flat_map{|o1, c1|
-          merge_options.map{|o2, c2|
-            [merge_pack_parts(o1, o2), c1 * c2]
-          }
-        }
-      else
-        raise "Unknown pack count type #{count.class}"
-      end
-    end
-    options
-  end
-
-  def build_pack_with_alternatives(pack_data, sheets)
-    if pack_data.all?{|k,v| v.is_a?(Integer)}
-      return build_simple_pack(pack_data, sheets)
-    end
-
-    options = resolve_option_combinations(pack_data)
-
-    if options.size == 1
-      # Not very likely, but technically possible
-      build_simple_pack(options[0][0], sheets)
-    else
-      gcd = options.map(&:last).inject(&:gcd)
-      WeightedPack.new(options.map{|subpack_data, chance|
-        [build_simple_pack(subpack_data, sheets), chance / gcd]
-      }.to_h)
-    end
   end
 
   def build_pack(set, full_variant)
@@ -134,17 +86,14 @@ class PackFactoryYaml
     (data.delete("sheets") || []).each{|sheet_name, sheet_data|
       sheets[sheet_name] = build_sheet_from_yaml_data(set_code, sheet_name, sheet_data)
     }
-    pack = case data.keys
-    when ["pack"]
-      build_pack_with_alternatives(data["pack"], sheets)
-    when ["packs"]
-      WeightedPack.new(data["packs"].map{|subpack_data|
-        chance = subpack_data.delete("chance")
-        subpack = build_pack_with_alternatives(subpack_data, sheets)
-        [subpack, chance]
-      }.to_h)
+    subpacks = data["packs"].map{|subpack_data, chance|
+      subpack = build_simple_pack(subpack_data, sheets)
+      [subpack, chance]
+    }
+    if subpacks.size == 1
+      pack = subpacks[0][0]
     else
-      raise "Unknown pack type #{data.keys.join(", ")}"
+      pack = WeightedPack.new(subpacks.to_h)
     end
 
     pack.set = set
