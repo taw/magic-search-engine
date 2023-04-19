effc8f1f35640a68a44f42c10ac78ef12a8c0783
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-04-19 15:15:40 +0100

    Spec fix

--- search-engine/spec/partner_spec.rb
+++ search-engine/spec/partner_spec.rb
@@ -2,11 +2,11 @@ describe "partner queries" do
   include_context "db"
 
   it "is:partner" do
-    assert_search_equal "is:partner -is:promo", %[
+    assert_search_equal "is:partner -is:promo -e:plist", %[
       ((o:"partner with") or
        (o:"partner" t:legendary t:creature) or
        (o:"partner" t:legendary t:planeswalker e:cmr))
-      -is:promo
+      -is:promo -e:plist
     ]
   end
 
