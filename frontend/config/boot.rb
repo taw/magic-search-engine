16fade42b5a6743642931801b19a6f7e88b344c5
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-04-29 00:22:14 +0100

    Preload database and boosters

--- frontend/config/boot.rb
+++ frontend/config/boot.rb
@@ -4,4 +4,9 @@ require "bundler/setup" # Set up gems listed in the Gemfile.
 
 # This should maybe be moved to a gem?
 require_relative "../../search-engine/lib/card_database"
+t0 = Time.now
 $CardDatabase = CardDatabase.load
+# Preload it
+$CardDatabase.supported_booster_types
+dt = Time.now - t0
+puts "Loading database took: #{dt.round(2)}s"
