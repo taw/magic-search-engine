59a2de9f76ec7901b029f23d8d82f006095073d3
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-05-12 23:06:06 +0100

    Fixed cards support for open_packs and open_packs_json CLI

--- search-engine/lib/sealed.rb
+++ search-engine/lib/sealed.rb
@@ -1,26 +1,41 @@
 class Sealed
   def initialize(db, *pack_descriptors)
-    factory = PackFactory.new(db)
+    @db = db
+    @factory = PackFactory.new(db)
+    @fixed = []
     @packs = []
     pack_descriptors.each do |descriptor|
-      if descriptor =~ /\A(\d+)x(.*)/
-        count = $1.to_i
-        set_code = $2
+      case descriptor
+      when %r[\A(\d+)x(.*/.*)]
+        add_card $1.to_i, $2
+      when %r[\A(.*/.*)]
+        add_card 1, descriptor
+      when /\A(\d+)x(.*)/
+        add_pack $1.to_i, $2
       else
-        count = 1
-        set_code = descriptor
+        add_pack 1, descriptor
       end
-      set_code, variant = set_code.split("-", 2)
-      pack = factory.for(set_code, variant)
-      unless pack
-        raise "No pack for set #{set_code}#{variant && " variant #{variant}"}"
-      end
-      @packs << [count, pack]
     end
   end
 
+  def add_pack(count, set_code)
+    set_code, variant = set_code.split("-", 2)
+    pack = @factory.for(set_code, variant)
+    unless pack
+      raise "No pack for set #{set_code}#{variant && " variant #{variant}"}"
+    end
+    @packs << [count, pack]
+  end
+
+  def add_card(count, description)
+    set_code, number, foil = description.split("/", 3)
+    set = @db.sets[set_code.downcase] or caise "Can't find set #{set_code}"
+    card = set.printings.find{|c| c.number.downcase == number.downcase} or raise "Can't find card #{set_code}/#{number}"
+    @fixed << PhysicalCard.for(card, foil == "foil")
+  end
+
   def call
-    cards = []
+    cards = @fixed.dup
     @packs.each do |count, pack|
       count.times do
         cards.push *pack.open
