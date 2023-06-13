85cdd5b6ba253ccde3a20dd630728ac316da1b8a
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-06-13 12:20:21 +0100

    Unicde normalization for accented characters in artist names and specs

---
+++ search-engine/spec/unicode_spec.rb
@@ -0,0 +1,33 @@
+describe "Unicode Normalization" do
+  include_context "db"
+
+  let(:letters_with_accents) do
+    db.printings.flat_map{|c|
+      [c.name, c.text, c.flavor, c.artist_name]
+        .join
+        .scan(/[\p{Letter}&&\p{Latin}&&[^a-zA-Z]]/)
+        .uniq
+    }.uniq.sort
+  end
+  let(:letters_in_all_cases) do
+    letters_with_accents.flat_map{|l| [l, l.upcase, l.downcase]}.uniq.sort
+  end
+
+  it "normalizes all Unicode characters" do
+    letters_in_all_cases.each do |letter|
+      case letter
+      when "œ"
+        letter.normalize_accents.should eq("oe")
+      when "Œ"
+        letter.normalize_accents.should eq("Oe")
+      else
+        letter.normalize_accents.should match(/\A[a-zA-Z]\z/)
+        letter.normalize_accents.should eq(letter.unicode_normalize(:nfd).gsub(/[^A-Za-z]/, ""))
+      end
+    end
+  end
+
+  it "deals with Polish letters" do
+    assert_search_equal "a:wuzyk", "a:wużyk"
+  end
+end
