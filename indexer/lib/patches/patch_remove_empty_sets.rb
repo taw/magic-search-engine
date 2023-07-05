e9a22132020a81a348f68f64668fba7728e5e8f8
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-07-05 21:39:15 +0100

    mtgjson update, some empty sets

--- indexer/lib/patches/patch_remove_empty_sets.rb
+++ indexer/lib/patches/patch_remove_empty_sets.rb
@@ -5,12 +5,10 @@
 class PatchRemoveEmptySets < Patch
   def call
     nonempty_sets = @cards.values.flatten(1).map{|x| x["set"]["code"] }.to_set
-    empty_sets_to_keep = %W[q07 q08].to_set
+    empty_sets_to_keep = %W[q01 q02 q03 q04 q05 q07 q08 dd3].to_set
 
     @sets.keep_if do |s|
       nonempty_sets.include?(s["code"]) or empty_sets_to_keep.include?(s["code"])
     end
-
-
   end
 end
