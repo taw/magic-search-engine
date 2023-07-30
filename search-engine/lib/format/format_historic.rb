69aea00414c991d30dbbed07de86374b8c8ee7eb
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-07-30 21:09:46 +0100

    First attempt at Alchemy and Alchemy-fixed Historic (they are not correct yet)

--- search-engine/lib/format/format_historic.rb
+++ search-engine/lib/format/format_historic.rb
@@ -3,7 +3,17 @@ class FormatHistoric < FormatVintage
     "Historic"
   end
 
+  def legality(card)
+    card = card.main_front if card.is_a?(PhysicalCard)
+    if !in_format?(card)
+      nil
+    else
+      @ban_list.legality(card.name, @time)
+    end
+  end
+
   def in_format?(card)
+    return false if card.has_alchemy
     card.printings.each do |printing|
       next if @time and printing.release_date > @time
       # These is currently one excluded set - XANA
