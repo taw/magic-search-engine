12c418b9b2d3231599ec4a127c1171b9c20729df
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-10-19 14:12:09 +0100

    Frame effects fixes

--- indexer/lib/patches/patch_mtgjson_versions.rb
+++ indexer/lib/patches/patch_mtgjson_versions.rb
@@ -259,6 +259,13 @@ class PatchMtgjsonVersions < Patch
         card["layout"] = "saga"
       end
 
+      # First https://github.com/mtgjson/mtgjson/issues/1094 but it keeps coming back
+      # so a permanent fix here
+      if card["frame_effects"]&.include?("borderless")
+        card["border"] = "borderless"
+        card["frame_effects"].delete("borderless")
+      end
+
       if card["name"] == "Aragorn, Hornburg Hero"
         # It's incorrectly parsed
         card["types"]  = ["Creature"]
