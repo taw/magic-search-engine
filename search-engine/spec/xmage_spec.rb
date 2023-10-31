5eb3a1a70b1978f51713b4d97ebd15a0144a8419
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-10-31 23:38:00 +0000

    Data update

--- search-engine/spec/xmage_spec.rb
+++ search-engine/spec/xmage_spec.rb
@@ -6,7 +6,7 @@ describe "XMage" do
     # Most recent sets often come to XMage late or in parts, so this -e: clause needs periodic updating
     #
     # Base LTR works, but there are extra cards being released post main release as part of LTR set
-    assert_search_results "f:modern (st:std or st:modern) -in:xmage -e:mat,woe,ltr",
+    assert_search_results "f:modern (st:std or st:modern) -in:xmage -e:mat,woe,ltr,lci",
       # mutate from graveyard
       "Brokkos, Apex of Forever",
       # text change
