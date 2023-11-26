15fb25bbfbe277332526b91367e58115d4d328fc
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-11-26 10:30:52 +0000

    Optimizing ConditionNumberRange

--- search-engine/lib/condition/condition_number_range.rb
+++ search-engine/lib/condition/condition_number_range.rb
@@ -3,22 +3,21 @@ class ConditionNumberRange < ConditionSimple
     @ranges = ranges.downcase.split(",").map{|range|
       a, b = range.split("-", 2)
       b ||= a
-      [[a.to_i, a], [b.to_i, b =~ /\D/ ? b : "#{b}zzz"], a == "set", b == "set"]
+      [a.to_i, a, b.to_i, b =~ /\D/ ? b : "#{b}zzz", a == "set", b == "set"]
     }
   end
 
   def match?(card)
-    card_number_s = card.number.downcase
     card_number_i = card.number_i
 
-    @ranges.each do |a, b, aset, bset|
+    @ranges.each do |ai, as, bi, bs, aset, bset|
       if aset
         base_set_size = card.set.base_set_size
         next unless base_set_size && (card_number_i >= base_set_size)
       else
-        next unless card_number_i >= a[0]
-        if card_number_i == a[0]
-          next unless card_number_s >= a[1]
+        next unless card_number_i >= ai
+        if card_number_i == ai
+          next unless card.number.downcase >= as
         end
       end
 
@@ -26,11 +25,12 @@ class ConditionNumberRange < ConditionSimple
         base_set_size = card.set.base_set_size
         next unless base_set_size && (card_number_i <= card.set.base_set_size)
       else
-        next unless card_number_i <= b[0]
-        if card_number_i == b[0]
-          next unless card_number_s <= b[1]
+        next unless card_number_i <= bi
+        if card_number_i == bi
+          next unless card.number.downcase <= bs
         end
       end
+
       return true
     end
 
@@ -38,7 +38,7 @@ class ConditionNumberRange < ConditionSimple
   end
 
   def to_s
-    "number:#{@ranges.map{|(ai, as), (bi, bs)|
+    "number:#{@ranges.map{|ai, as, bi, bs, aset, bset|
       if as == bs
         as
       else
