65dee30594d88b5516ccfa7985e00fdb5bc3413b
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-11-26 09:40:56 +0000

    Optimizing ConditionNumberRange

--- search-engine/lib/condition/condition_number_range.rb
+++ search-engine/lib/condition/condition_number_range.rb
@@ -10,22 +10,31 @@ class ConditionNumberRange < ConditionSimple
   def match?(card)
     card_number_s = card.number.downcase
     card_number_i = card.number_i
-    key = [card_number_i, card_number_s]
-    @ranges.any? do |a, b, aset, bset|
+
+    @ranges.each do |a, b, aset, bset|
       if aset
         base_set_size = card.set.base_set_size
         next unless base_set_size && (card_number_i >= base_set_size)
       else
-        next unless (key <=> a) >= 0
+        next unless card_number_i >= a[0]
+        if card_number_i == a[0]
+          next unless card_number_s >= a[1]
+        end
       end
 
       if bset
         base_set_size = card.set.base_set_size
-        base_set_size && (card_number_i <= card.set.base_set_size)
+        next unless base_set_size && (card_number_i <= card.set.base_set_size)
       else
-        (key <=> b) <= 0
+        next unless card_number_i <= b[0]
+        if card_number_i == b[0]
+          next unless card_number_s <= b[1]
+        end
       end
+      return true
     end
+
+    false
   end
 
   def to_s
