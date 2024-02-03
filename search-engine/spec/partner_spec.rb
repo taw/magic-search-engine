0b677bb86e417428ae39b6f2e606a19a289b05d8
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2024-02-03 18:08:43 +0000

    Remove some old plist stuff

--- search-engine/spec/partner_spec.rb
+++ search-engine/spec/partner_spec.rb
@@ -2,11 +2,11 @@ describe "partner queries" do
   include_context "db"
 
   it "is:partner" do
-    assert_search_equal "is:partner -is:promo -e:plist", %[
+    assert_search_equal "is:partner -is:promo -e:plst", %[
       ((o:"partner with") or
        (o:"partner" t:legendary t:creature) or
        (o:"partner" t:legendary t:planeswalker e:cmr))
-      -is:promo -e:plist
+      -is:promo -e:plst
     ]
   end
 
