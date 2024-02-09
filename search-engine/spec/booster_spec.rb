3b4434e01b18b26ae420d0d6eec690e1034a7e03
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2024-02-09 01:18:09 +0000

    Fixed some specs

--- search-engine/spec/booster_spec.rb
+++ search-engine/spec/booster_spec.rb
@@ -70,12 +70,14 @@ describe "is:booster" do
           pack_factory.for(set_code, "arena").should_not(be_nil, "#{set_code} should have Arena boosters")
           pack_factory.for(set_code, nil).should(be_nil, "#{set_code} should not have regular boosters")
         elsif standard_arena_sets.include?(set_code)
-          pack_factory.for(set_code, "arena").should_not(be_nil, "#{set_code} should have Arena boosters")
           # MKM
           if set.release_date >=  Date.parse("2024-02-09")
+            # not sure if these should be xxx-play-arena or just xxx-arena
+            pack_factory.for(set_code, "play-arena").should_not(be_nil, "#{set_code} should have Arena Play boosters")
             pack_factory.for(set_code, "play").should_not(be_nil, "#{set_code} should have regular boosters")
             pack_factory.for(set_code, nil).should(be_nil, "#{set_code} should not have regular boosters")
           else
+            pack_factory.for(set_code, "arena").should_not(be_nil, "#{set_code} should have Arena boosters")
             pack_factory.for(set_code, nil).should_not(be_nil, "#{set_code} should have regular boosters")
           end
         else
