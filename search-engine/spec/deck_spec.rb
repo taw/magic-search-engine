64cbbcdb684eddd8335e5b729272f80d2f33f0ee
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2024-04-26 11:43:53 +0100

    Some spec fixes

--- search-engine/spec/deck_spec.rb
+++ search-engine/spec/deck_spec.rb
@@ -130,12 +130,14 @@ describe Deck do
     end
   end
 
+  # This is basically false now and every XXX set now has XXc associated with it
+  # It's arguably useful for checking no really weird card leaks
   it "cards in precon sets have no off-set cards" do
     precon_sets.each do |set|
       sets_found = set.decks.flat_map(&:physical_cards).map(&:set).map(&:code).uniq
-      # Contains some Amonkhet cards
       case set.code
       when "e01"
+        # Contains some Amonkhet cards
         sets_found.should match_array ["e01", "akh", "oe01"]
       when "hop"
         sets_found.should match_array ["hop", "ohop"]
@@ -193,6 +195,8 @@ describe Deck do
         sets_found.should match_array ["c18", "oc18"]
       when "c19"
         sets_found.should match_array ["c19", "oc19"]
+      when "otc"
+        sets_found.should match_array ["otc", "otj"]
       else
         sets_found.should eq [set.code]
       end
