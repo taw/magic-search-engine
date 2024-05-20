25c69556eb763865741253dc3393020400ff1685
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2024-05-20 10:12:55 +0100

    is:stickers and is:attraction

--- search-engine/lib/ban_list/vintage.rb
+++ search-engine/lib/ban_list/vintage.rb
@@ -381,6 +381,7 @@ BanList.for_format("vintage") do
     "Finishing Move" => "banned",
     "Glitterflitter" => "banned",
     "_____ Goblin" => "banned",
+    '"Name Sticker" Goblin' => "banned", # not explicitly, it's just MTGO variant
     "Last Voyage of the _____" => "banned",
     "Lineprancers" => "banned",
     "Make a _____ Splash" => "banned",
