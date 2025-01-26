9c57e299a95b7462696bce98b21b48aa66e1ce67
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2025-01-26 15:02:31 +0000

    Data update

--- search-engine/spec/xmage_spec.rb
+++ search-engine/spec/xmage_spec.rb
@@ -4,7 +4,7 @@ describe "XMage" do
   it "has almost all Modern legal cards" do
     # st:modern to ignore reprints in promo sets
     # Most recent sets often come to XMage late or in parts, so this -e: clause needs periodic updating
-    assert_search_results "f:modern (st:std or st:modern) -in:xmage -e:acr,blb,dsk,fdn",
+    assert_search_results "f:modern (st:std or st:modern) -in:xmage -e:acr,blb,dsk,fdn,dft",
       # mutate from graveyard
       "Brokkos, Apex of Forever",
       # text change
