b9c8de197a39c866f8ecae851544acc5d8b68f09
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2025-05-12 08:25:54 +0100

    Use token faceName

--- indexer/lib/token_uuids_serializer.rb
+++ indexer/lib/token_uuids_serializer.rb
@@ -9,9 +9,19 @@ class TokenUuidsSerializer
 
   def to_s
     @tokens
-      .map{|token| [token["set"]["code"], token["setCode"]&.downcase, token["number"], token["uuid"], token["name"]] }
+      .map{|token| token_data(token) }
       .sort_by{|sc,n,u,name| [sc, n.to_i, n, name, u || ""] }
       .map{|row| row.join("\t") + "\n" }
       .join
+  end
+
+  def token_data(token)
+    [
+      token["set"]["code"],
+      token["setCode"]&.downcase,
+      token["number"],
+      token["uuid"],
+      token["faceName"] || token["name"],
+    ]
   end
 end
