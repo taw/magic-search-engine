afdc4667eb8322518875a0567f155a690ea22f18
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2025-05-27 10:45:50 +0100

    Performance improvements and typo fix

--- search-engine/lib/condition/condition_is_baseset.rb
+++ search-engine/lib/condition/condition_is_baseset.rb
@@ -1,10 +1,6 @@
 class ConditionIsBaseset < ConditionSimple
   def match?(card)
-    return false if card.variant_foreign or card.variant_misprint or card.promo_types&.include?("reversibleback")
-    base_set_size = card.set.base_set_size
-    return false unless base_set_size
-    number_i = card.number_i
-    number_i >= 1 and number_i <= base_set_size
+    card.baseset?
   end
 
   def to_s
