8cad2f584374fcede1012be5703447eff465c89c
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2025-09-14 21:37:06 +0100

    produces: support

--- search-engine/lib/condition/condition_produces.rb
+++ search-engine/lib/condition/condition_produces.rb
@@ -1,14 +1,31 @@
 class ConditionProduces < ConditionSimple
   def initialize(op, mana)
     @op = op
-    @mana = mana
+    @mana = mana.downcase.chars.grep(/[wubrgc]/).to_set
   end
 
   def match?(card)
-    true
+    card_produces = card.produces&.chars&.to_set || Set[]
+
+    case @op
+    when ">="
+      card_produces >= @mana
+    when ">"
+      card_produces > @mana
+    when "="
+      card_produces == @mana
+    when "!="
+      card_produces != @mana
+    when "<"
+      card_produces < @mana
+    when "<="
+      card_produces <= @mana
+    else
+      raise "Unrecognized comparison #{@op}"
+    end
   end
 
   def to_s
-    "produces#{@op}#{@mana}"
+    "produces#{@op}#{@mana.join}"
   end
 end
