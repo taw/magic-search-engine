4255e8061a0265d3acf27e7e8b709903bbe3612c
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2025-09-20 22:57:11 +0100

    Updated mana font and symbols support

--- search-engine/spec/mana_cost_spec.rb
+++ search-engine/spec/mana_cost_spec.rb
@@ -13,4 +13,14 @@ describe "mana cost queries" do
   it "!=" do
     assert_search_equal "mana!=2r", "-mana=2r"
   end
+
+  it "2/x" do
+    assert_search_results "mana={2/r}{2/w}{2/b}",
+      "Defibrillating Current",
+      "Reigning Victor"
+  end
+
+  it "c/x" do
+    assert_search_results "mana>={c/w}", "Ulalek, Fused Atrocity"
+  end
 end
