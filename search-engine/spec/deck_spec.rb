b3696a6b48e380fe02c97b81429ee419b429f5a1
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2025-09-29 13:33:10 +0100

    Demo decks fix

--- search-engine/spec/deck_spec.rb
+++ search-engine/spec/deck_spec.rb
@@ -74,6 +74,7 @@ describe Deck do
       ["standard", "Historic Brawl Precon Deck"],
       ["shandalar", "Shandalar Enemy Deck"], # assigned to PAST, as there's no Shandalar set
       ["core", "Jumpstart"], # FDN
+      ["portal", "Demo Deck"],
       # Non-decks, this needs to be sorted out at some point
       ["box", "Box"],
       ["sld", "Secret Lair Drop"],
