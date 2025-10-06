d2d39a19793531fb4cba4ebed6b1ef2fe963f7ec
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2025-10-06 14:39:59 +0100

    Language override list

--- indexer/lib/patches/patch_set_languages.rb
+++ indexer/lib/patches/patch_set_languages.rb
@@ -19,9 +19,49 @@ class PatchSetLanguages < Patch
     "Spanish" => "sp",
   }
 
+  OVERRIDE = {
+    # Promos in non-regular language
+    "pinv" => ["en"], # Latin
+    "ppls" => ["en"], # Ancient Greek
+    "papc" => ["en"], # Sanskrit
+    "pody" => ["en"], # Arabic
+    "pjud" => ["en"], # Hebrew
+    "j14" => ["en"], # Phyrexian
+    "pw23" => ["en"], # Phyrexian
+
+    # No good source
+    "mb1" => ["en"],
+
+    # https://magic.wizards.com/en/news/feature/challenger-decks-lists-2018-02-23
+    "q01 Challenger Decks 2018" => ["en", "jp"],
+
+    # https://magic.wizards.com/en/news/feature/challenger-decks-2019-03-18
+    # These decks will be available worldwide in English, with Japanese available in Japan.
+    "q02 Challenger Decks 2019" => ["en"],
+    "q03 Challenger Decks 2019 Japan" => ["jp"],
+
+    # https://magic.wizards.com/en/news/feature/challenger-decks-2020-02-15
+    # These decks will be available worldwide in English, with Japanese available in Japan.
+    "q04 Challenger Decks 2020" => ["en", "jp"],
+
+    # https://magic.wizards.com/en/news/feature/challenger-decks-2021-02-12
+    # These decks will be available worldwide in English, with Japanese available in Japan.
+    "q05 Challenger Decks 2021" => ["en", "jp"],
+
+    # https://magic.wizards.com/en/news/feature/challenger-decks-2022
+    # These latest Challenger Decks will be available worldwide in English, with German, French, and Japanese languages available in their respective regions.
+    "q08 Pioneer Challenger Decks 2022" => ["en", "de", "fr", "jp"],
+  }.to_h{|sc,ls| [sc.split.first, ls]}
+
   def call
     each_set do |set|
+      set_code = set["code"]
       set_desc = "Set #{set["code"]} #{set["name"]}"
+
+      if OVERRIDE[set_code]
+        set["languages"] = OVERRIDE[set_code]
+        next
+      end
 
       if set["languages"].nil?
         warn "#{set_desc} has no language information. Defaulting to English-only."
