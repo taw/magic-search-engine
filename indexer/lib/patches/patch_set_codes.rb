1e1a1502078900ff26b0cf4c7297a2af520ccba4
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2026-08-06 11:56:44 +0100

    Support some Arena set codes

--- indexer/lib/patches/patch_set_codes.rb
+++ indexer/lib/patches/patch_set_codes.rb
@@ -3,8 +3,9 @@ class PatchSetCodes < Patch
     each_set do |set|
       set["code"] = set["official_code"]&.downcase
 
-      # magiccards.info uses different codes for some sets, expose them as alternative_code
-      alternative_code = mci_codes[set["code"]]
+      # magiccards.info and MTG Arena use different codes for some sets,
+      # expose them as alternative_code
+      alternative_code = mci_codes[set["code"]] || arena_codes[set["code"]]
       if alternative_code and alternative_code != set["code"]
         set["alternative_code"] = alternative_code
       end
@@ -20,15 +21,46 @@ class PatchSetCodes < Patch
       raise "There are duplicated set codes: #{duplicated_codes.keys}"
     end
 
+    check_alternative_codes!
+
     each_printing do |card|
       card["set_code"] = card["set"]["code"]
     end
   end
 
-  def mci_codes
-    @mci_codes ||= begin
-      mci_codes_path = Indexer::ROOT + "mci_set_codes.txt"
-      mci_codes_path.readlines.map(&:chomp).map{|x| x.split}.to_h
+  # There's only one alternative_code slot per set, and resolve_editions prefers
+  # real codes over alternative ones, so any of these would silently lose a mapping.
+  # Both files contain entries for sets we no longer have, so only check what got applied.
+  def check_alternative_codes!
+    codes = @sets.map{|s| s["code"]}
+    both = codes.select{|c| mci_codes[c] and arena_codes[c]}
+    unless both.empty?
+      raise "There are sets with both mci and arena codes: #{both}"
     end
+
+    alternative_codes = @sets.filter_map{|s| s["alternative_code"]}
+    shadowed = alternative_codes & codes
+    unless shadowed.empty?
+      raise "There are alternative set codes which are also real set codes: #{shadowed}"
+    end
+
+    duplicated = alternative_codes.tally.select{|_,v| v > 1}
+    unless duplicated.empty?
+      raise "There are duplicated alternative set codes: #{duplicated.keys}"
+    end
+  end
+
+  def mci_codes
+    @mci_codes ||= read_codes("mci_set_codes.txt")
+  end
+
+  # Arena renames a handful of sets. It also lumps every Alchemy set into
+  # Y22..Y26, and that many-to-one mapping can't be expressed here.
+  def arena_codes
+    @arena_codes ||= read_codes("arena_set_codes.txt")
+  end
+
+  def read_codes(file_name)
+    (Indexer::ROOT + file_name).readlines.map(&:chomp).map{|x| x.split}.to_h
   end
 end
--- search-engine/spec/set_codes_spec.rb
+++ search-engine/spec/set_codes_spec.rb
@@ -20,6 +20,26 @@ describe "Set Codes" do
     end
   end
 
+  # Arena's codes go into the same alternative_code slot as the mci ones.
+  # Arena also lumps every Alchemy set into Y22..Y26, and one code per set can't express that.
+  it "arena_code" do
+    {
+      "dar" => "dom",
+      "aha1" => "ha1",
+      "aha2" => "ha2",
+      "aha3" => "ha3",
+      "aha4" => "ha4",
+      "aha5" => "ha5",
+      "aha6" => "ha6",
+      "aha7" => "ha7",
+    }.each do |arena_code, set_code|
+      set = db.sets[set_code]
+      set.alternative_code.should eq(arena_code)
+      # "dar" alone would name-match The Dark and Darksteel
+      assert_resolves arena_code, set
+    end
+  end
+
   def assert_resolves(query, *sets)
     sets.map(&:name).to_set.should eq(db.resolve_editions(query).map(&:name).to_set)
   end
