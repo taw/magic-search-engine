ccba4d118fb64dfed67bdda20249d03131c7e8f6
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2026-08-11 14:11:17 +0100

    More foreign name fixes

--- indexer/lib/patches/patch_reconcile_foreign_names.rb
+++ indexer/lib/patches/patch_reconcile_foreign_names.rb
@@ -2,6 +2,9 @@ class PatchReconcileForeignNames < Patch
   # Japanese cards print furigana next to kanji, and some entries include them
   # like 事（じ）件（けん）現（げん）場（ば）. A few even got cut in half like 用心じん）棒、ラクドス.
   FURIGANA_RX = /（[ぁ-ゖァ-ヺー]*）|（[ぁ-ゖァ-ヺー]*|[ぁ-ゖァ-ヺー]*）/
+  # Usually "x // y", but some data has "x/y", and SP//dr is not a split card
+  SPLIT_RX = %r{ // |/}
+  LIGATURES = {"œ" => "oe", "Œ" => "Oe", "æ" => "ae", "Æ" => "Ae"}.freeze
 
   def language_name_to_code
     @language_name_to_code ||= {
@@ -16,6 +19,20 @@ class PatchReconcileForeignNames < Patch
       "Russian"             => "ru",
       "Spanish"             => "sp",
     }
+  end
+
+  # Gatherer's French pages for TDM showcase omens have their fields shifted by one,
+  # so the back face got named after the front face's first line of rules text
+  def known_wrong_names
+    @known_wrong_names ||= Set[
+      ["Absorb Essence", "fr", "Vol"],
+      ["Charring Bite", "fr", "Vol"],
+      ["Chilling Screech", "fr", "Vol"],
+      ["Dusk Sight", "fr", "Vol, contact mortel"],
+      ["Flush Out", "fr", "Vol, célérité"],
+      ["Petty Revenge", "fr", "Vol"],
+      ["Roost Seek", "fr", "Vol"],
+    ]
   end
 
   def known_promo_language
@@ -82,7 +99,9 @@ class PatchReconcileForeignNames < Patch
     result = {}
 
     raw_data.each do |language_code, names|
+      names = names.reject{|name, _| known_wrong_names.include?([card_name, language_code, name])}
       names = drop_split_card_names(card_name, language_code, names)
+      names = drop_english_names(card_name, names)
       # Every name we had for that language was junk
       next if names.empty?
       result[language_code] = reconcile_language(card_name, names)
@@ -95,33 +114,56 @@ class PatchReconcileForeignNames < Patch
   # sometimes even the English "x // y", which is never what we want.
   def drop_split_card_names(card_name, language_code, names)
     names.reject do |name, _|
-      parts = name.split(" // ")
-      next false if parts.size < 2
+      parts = split_card_name(name) or next false
       # English name of the whole card
       next true if parts.all?{|part| english_card_name?(part)}
       # We already have the half we want
       next true if parts.any?{|part| names.key?(part)}
+      # Or at least some name that isn't glued together
+      next true if names.any?{|other, _| other != name and !split_card_name(other) and !english_card_name?(other)}
       warn "Only have both halves of split card #{card_name} [#{language_code}]: #{name}"
       false
     end
   end
 
+  # Gatherer often lists the English name as a foreign name,
+  # sometimes with fancy quotes like Kongming, “Sleeping Dragon”
+  def drop_english_names(card_name, names)
+    result = names.reject{|name, _| normalize_quotes(name) == card_name}
+    # Plenty of cards genuinely have the same name in English and in other languages
+    result.empty? ? names : result
+  end
+
+  def split_card_name(name)
+    parts = name.split(SPLIT_RX, -1)
+    return nil if parts.size < 2 or parts.any?(&:empty?)
+    parts
+  end
+
+  def normalize_quotes(name)
+    name.tr("“”", %[""])
+  end
+
   def english_card_name?(name)
     @english_card_names ||= Set.new(@cards.keys)
-    @english_card_names.include?(name)
+    @english_card_names.include?(normalize_quotes(name))
+  end
+
+  # French really uses œ (cœur, œuf, rancœur), so OE is just mangled data.
+  # It doesn't use æ at all, that's mangled data for ae in faerie or aether.
+  # Otherwise the most capitalized name is the one that looks like a card name.
+  def best_spelling(names)
+    names.max_by{|name|
+      [name.count("œŒ"), -name.count("æÆ"), name.chars.count{|c| c.upcase == c}]
+    }
   end
 
   def reconcile_language(card_name, names)
     if names.size == 1
       [names.keys[0]]
-    elsif names.size == 2 and names.include?(card_name)
-      # English names are often mistakenly listed by Gatherer
-      [(names.keys - [card_name])[0]]
-    elsif names.keys.map(&:upcase).uniq.size == 1
-      # If names differ only by capitalization, take the most capitalized name
-      [names.keys.max_by{|s|
-        s.chars.count{|c| c.upcase == c}
-      }]
+    elsif names.keys.map{|n| n.gsub(/[œŒæÆ]/, LIGATURES).upcase}.uniq.size == 1
+      # If names differ only by capitalization or ligatures, take the best spelling
+      [best_spelling(names.keys)]
     elsif names.keys.any?{|n| n =~ /\(/}
       # Pointless A (A/B) for split cards
       short_names = names.keys.map{|x| x.sub(/\s*\(.*\)\z/, "")}.uniq - [card_name]
--- search-engine/spec/indexer_fixes_spec.rb
+++ search-engine/spec/indexer_fixes_spec.rb
@@ -99,12 +99,18 @@ describe "Indexer Fixes Test" do
   end
 
   context "foreign names" do
+    # Same as the indexer, and SP//dr is not a split card
+    def split_card_name(name)
+      parts = name.split(%r{ // |/}, -1)
+      parts if parts.size > 1 and parts.none?(&:empty?)
+    end
+
     def foreign_names_where
       result = []
       db.cards.each_value do |card|
         card.foreign_names.each do |language, names|
           names.each do |name|
-            result << [card.name, language, name] if yield(name, names)
+            result << [card.name, language, name] if yield(name, names, card)
           end
         end
       end
@@ -114,13 +120,19 @@ describe "Indexer Fixes Test" do
     it "No English names of split cards" do
       english_names = Set[*db.cards.each_value.map(&:name)]
       foreign_names_where{|name, _|
-        name.split(" // ").then{|parts| parts.size > 1 and parts.all?{|part| english_names.include?(part)}}
+        split_card_name(name)&.all?{|part| english_names.include?(part)}
       }.should eq []
     end
 
-    it "No both halves of split cards when we have the half we want" do
+    it "No both halves of split cards when we have some better name" do
       foreign_names_where{|name, names|
-        name.split(" // ").then{|parts| parts.size > 1 and parts.any?{|part| names.include?(part)}}
+        split_card_name(name) and names.size > 1
+      }.should eq []
+    end
+
+    it "No English names when we have an actual translation" do
+      foreign_names_where{|name, names, card|
+        names.size > 1 and name.tr("“”", %[""]) == card.name
       }.should eq []
     end
 
@@ -132,11 +144,36 @@ describe "Indexer Fixes Test" do
       db.cards["warden"].foreign_names[:de].should eq ["Vollstrecker"]
       db.cards["find"].foreign_names[:sp].should eq ["Descubrir"]
       db.cards["dawn"].foreign_names[:cs].should eq ["朝生"]
+      db.cards["supply"].foreign_names[:it].should eq ["Offerta"]
+      db.cards["replicate"].foreign_names[:cs].should eq ["复造"]
+    end
+
+    it "SP//dr is not a split card" do
+      db.cards["sp//dr, piloted by peni"].foreign_names[:it].should eq ["SP//dr, Aracnomecha"]
     end
 
     it "Japanese names have no furigana" do
       db.cards["aftermath analyst"].foreign_names[:jp].should eq ["事件現場の分析者"]
       db.cards["rakdos, the muscle"].foreign_names[:jp].should eq ["用心棒、ラクドス"]
+    end
+
+    it "French names use œ but not æ" do
+      db.cards["rancor"].foreign_names[:fr].should eq ["Rancœur"]
+      db.cards["dingus egg"].foreign_names[:fr].should eq ["Œuf de dingus"]
+      db.cards["faerie mastermind"].foreign_names[:fr].should eq ["Érudit faerie"]
+      db.cards["cloud of faeries"].foreign_names[:fr].should eq ["Nuée de faeries"]
+    end
+
+    it "English names with fancy quotes are still English names" do
+      db.cards[%[kongming, "sleeping dragon"]].foreign_names[:cs].should eq ["卧龙先生诸葛亮"]
+    end
+
+    it "No rules text instead of names on TDM omens" do
+      db.cards["absorb essence"].foreign_names[:fr].should eq ["Absorption d'essence"]
+      db.cards["flush out"].foreign_names[:fr].should eq ["Purge"]
+      db.cards["dusk sight"].foreign_names[:fr].should eq ["Vision crépusculaire"]
+      # Which is a real card name, just not of these
+      db.cards["flight"].foreign_names[:fr].should eq ["Vol"]
     end
   end
 end
