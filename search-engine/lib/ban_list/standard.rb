7eba4c8c5dc27f1825c8d601f97fd704a7d10859
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2026-08-12 14:38:51 +0100

    Darksteel Citadel banlist expired with rotation tweak

--- search-engine/lib/ban_list/standard.rb
+++ search-engine/lib/ban_list/standard.rb
@@ -117,8 +117,11 @@ BanList.for_format("standard") do
     "https://magic.wizards.com/en/articles/archive/feature/june-20-2011-dci-banned-restricted-list-announcement-2011-06-20",
     "Jace, the Mind Sculptor" => "banned",
     "Stoneforge Mystic" => "banned",
-    # OK, this is awkward, was it ever "unbanned" ?
-    # it just rotated out of Standard and banlist, then got reprinted as legal
+  )
+
+  change(
+    "2014-07-18",
+    "Original ban expired with rotation, reprinted into Standard as a legal card",
     "Darksteel Citadel" => "legal",
   )
 
