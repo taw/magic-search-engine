be7b549d6f79c853157520e52d6b9cff41432632
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2026-09-01 08:17:53 +0100

    SLZ excluded from Pauper legality

--- search-engine/lib/format/format_vintage.rb
+++ search-engine/lib/format/format_vintage.rb
@@ -14,6 +14,9 @@ class FormatVintage < Format
     # This could be done programmatically instead
     #
     # Pauper format needs it for defining what counts as a "common"
+    #
+    # SLZ excluded from Pauper legality https://bsky.app/profile/gavinverhey.bsky.social/post/3mtu55rdm4c2f
+    # (it's here because it's just reprints and doesn't affect any other format)
 
     excluded_sets = Set[*%w[
       30a
@@ -40,6 +43,7 @@ class FormatVintage < Format
       rin
       sir
       sis
+      slz
       tafr
       tclb
       tltr
